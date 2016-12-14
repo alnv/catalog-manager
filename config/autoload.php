b44ae5b177b82d4d455915371f170a0dc1ead95a
Alexander Naumov <me@alexandernaumov.de>
2016-12-14 16:55:07 +0100

    add more whitespace

--- config/autoload.php
+++ config/autoload.php
@@ -5,12 +5,11 @@ if ( class_exists( 'NamespaceClassLoader' ) ) {
     NamespaceClassLoader::add( 'oceanCatalog', 'system/modules/oceanCatalog/library' );
 }
 
-if (class_exists('NamespaceClassLoader')) {
+if ( class_exists( 'NamespaceClassLoader' ) ) {
 
-    NamespaceClassLoader::addClassMap(array
-    (
+    NamespaceClassLoader::addClassMap([
 
         'oceanCatalog\tl_catalog' => 'system/modules/oceanCatalog/classes/tl_catalog.php',
         'oceanCatalog\tl_catalog_fields' => 'system/modules/oceanCatalog/classes/tl_catalog_fields.php',
-    ));
+    ]);
 }

5802dca75992bbbef2e0fcf546a1204e4b0fd0b4
Alexander Naumov <me@alexandernaumov.de>
2016-12-15 14:58:53 +0100

    tidy up code

--- config/config.php
+++ config/config.php
@@ -5,5 +5,9 @@ $GLOBALS['BE_MOD']['system']['oceanCatalog'] = array(
 
     'name' => 'oceanCatalog',
 
-    'tables' => [ 'tl_catalog', 'tl_catalog_fields' ]
+    'tables' => [
+        
+        'tl_catalog',
+        'tl_catalog_fields'
+    ]
 );

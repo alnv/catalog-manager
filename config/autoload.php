f92318126e1cbbd1711988a7e8b3d767503c3868
Alexander Naumov <me@alexandernaumov.de>
2016-12-15 14:57:56 +0100

    change namespace

--- config/autoload.php
+++ config/autoload.php
@@ -2,14 +2,14 @@
 
 if ( class_exists( 'NamespaceClassLoader' ) ) {
 
-    NamespaceClassLoader::add( 'oceanCatalog', 'system/modules/oceanCatalog/library' );
+    NamespaceClassLoader::add( 'OceanCatalog', 'system/modules/oceanCatalog/library' );
 }
 
 if ( class_exists( 'NamespaceClassLoader' ) ) {
 
     NamespaceClassLoader::addClassMap([
 
-        'oceanCatalog\tl_catalog' => 'system/modules/oceanCatalog/classes/tl_catalog.php',
-        'oceanCatalog\tl_catalog_fields' => 'system/modules/oceanCatalog/classes/tl_catalog_fields.php',
+        'OceanCatalog\tl_catalog' => 'system/modules/oceanCatalog/classes/tl_catalog.php',
+        'OceanCatalog\tl_catalog_fields' => 'system/modules/oceanCatalog/classes/tl_catalog_fields.php',
     ]);
 }

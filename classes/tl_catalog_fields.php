762b85e4d1169e50e6ca226e4a9b6e1163411a88
Alexander Naumov <me@alexandernaumov.de>
2016-12-15 14:57:45 +0100

    add fields for input attributes

--- classes/tl_catalog_fields.php
+++ classes/tl_catalog_fields.php
@@ -1,3 +1,33 @@
 <?php
 
-class tl_catalog_fields extends \Backend {}
+namespace OceanCatalog;
+
+class tl_catalog_fields extends \Backend {
+
+    public function getFieldTypes() {
+
+        return [
+
+            'text',
+            'textarea',
+            'select',
+            'radio',
+            'checkbox',
+            'upload',
+            'fieldset',
+            'message'
+        ];
+    }
+
+    public function getInputTypes() {
+
+        return [
+
+            'text',
+            'hidden',
+            'url',
+            'number',
+            'date'
+        ];
+    }
+}

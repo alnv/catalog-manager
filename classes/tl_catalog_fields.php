27ab71b905f5020133216702f80b47d63442834f
Alexander Naumov <me@alexandernaumov.de>
2016-12-20 13:28:44 +0100

    add to functions

--- classes/tl_catalog_fields.php
+++ classes/tl_catalog_fields.php
@@ -9,25 +9,76 @@ class tl_catalog_fields extends \Backend {
         return [
 
             'text',
+            'text',
+            'date',
+            'hidden',
+            'number',
             'textarea',
             'select',
             'radio',
             'checkbox',
             'upload',
-            'fieldset',
-            'message'
+            'message',
+            'fieldsetStart',
+            'fieldsetStop'
         ];
     }
 
-    public function getInputTypes() {
+    public function getRGXPTypes() {
 
         return [
 
-            'text',
-            'hidden',
+            'alias',
+            'alnum',
+            'alpha',
+            'date',
+            'datim',
+            'digit',
+            'email',
+            'emails',
+            'extnd',
+            'folderalias',
+            'friendly',
+            'language',
+            'locale',
+            'natural',
+            'phone',
+            'prcnt',
             'url',
-            'number',
-            'date'
+            'time'
         ];
+    }
+
+    public function getRichTextEditor() {
+
+        return [
+
+            'tinyMCE',
+            'tinyFlash'
+        ];
+    }
+
+    public function getSQLStatements() {
+
+        return [
+
+            'c256' => "varchar(256) NOT NULL default ''",
+
+            'c1' => "char(1) NOT NULL default ''",
+            'c16' => "varchar(16) NOT NULL default ''",
+            'c32' => "varchar(32) NOT NULL default ''",
+            'c64' => "varchar(64) NOT NULL default ''",
+            'c128' => "varchar(128) NOT NULL default ''",
+            'c512' => "varchar(512) NOT NULL default ''",
+            'c1024' => "varchar(1024) NOT NULL default ''",
+            'c2048' => "varchar(2048) NOT NULL default ''",
+            'i5' => "smallint(5) unsigned NOT NULL default '0'",
+            'i10' => "int(10) unsigned NOT NULL default '0'"
+        ];
+    }
+
+    public function getCatalogFieldList( $arrRow ) {
+
+        return $arrRow['title'];
     }
 }

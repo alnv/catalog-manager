db8ac6fccdeed651b688a73cc323e67398844f50
Alexander Naumov <me@alexandernaumov.de>
2017-01-10 10:25:22 +0100

    add getter functions

--- classes/tl_catalog.php
+++ classes/tl_catalog.php
@@ -2,4 +2,58 @@
 
 namespace CatalogManager;
 
-class tl_catalog extends \Backend {}
+class tl_catalog extends \Backend {
+
+    public function getModeTypes () {
+
+        return [
+
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5'
+        ];
+    }
+
+    public function getFlagTypes() {
+
+        return [
+
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            '11',
+            '12'
+        ];
+    }
+
+    public function getDataContainerFields() {
+
+        return [];
+    }
+
+    public function getParentFields() {
+
+        // get fields from CatalogManager
+        return [];
+    }
+
+    public function getNavigationAreas() {
+
+        return [];
+    }
+
+    public function getNavigationPlace() {
+
+        return [];
+    }
+}

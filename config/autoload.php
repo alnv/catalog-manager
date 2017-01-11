a30a463106d32488f7990f4697026bb5b13c7e83
Alexander Naumov <me@alexandernaumov.de>
2017-01-11 13:47:43 +0100

    add classes

--- config/autoload.php
+++ config/autoload.php
@@ -1,16 +1,19 @@
 <?php
 
-if ( class_exists( 'NamespaceClassLoader' ) ) {
+if ( class_exists( 'ClassLoader' ) ) {
 
-    NamespaceClassLoader::add( 'CatalogManager', 'system/modules/catalog-manager/library' );
+    ClassLoader::addNamespace( 'CatalogManager' );
 }
 
 if ( class_exists( 'NamespaceClassLoader' ) ) {
 
     NamespaceClassLoader::addClassMap([
 
+        'CatalogManager\Toolkit' => 'system/modules/catalog-manager/library/alnv/Toolkit.php',
         'CatalogManager\tl_catalog' => 'system/modules/catalog-manager/classes/tl_catalog.php',
+        'CatalogManager\SQLBuilder' => 'system/modules/catalog-manager/library/alnv/SQLBuilder.php',
         'CatalogManager\CatalogManager' => 'system/modules/catalog-manager/library/alnv/CatalogManager.php',
         'CatalogManager\tl_catalog_fields' => 'system/modules/catalog-manager/classes/tl_catalog_fields.php',
+        'CatalogManager\InitializeSystem' => 'system/modules/catalog-manager/library/alnv/InitializeSystem.php',
     ]);
 }

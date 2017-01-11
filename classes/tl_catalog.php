54f19c1484bd1f77ca1c4a76ee8cc55b626dc495
Alexander Naumov <me@alexandernaumov.de>
2017-01-11 13:49:15 +0100

    implement contao navigation getter

--- classes/tl_catalog.php
+++ classes/tl_catalog.php
@@ -49,7 +49,33 @@ class tl_catalog extends \Backend {
 
     public function getNavigationAreas() {
 
-        return [];
+        $arrModules = $GLOBALS['BE_MOD'] ? $GLOBALS['BE_MOD'] : [];
+        $arrReturn = [];
+
+        if ( !is_array( $arrModules ) ) {
+
+            return [];
+        }
+
+        foreach ( $arrModules as $strName => $arrModule ) {
+
+            $arrLabel = $GLOBALS['TL_LANG']['MOD'][$strName];
+            $strModuleName = $strName;
+
+            if ( $arrLabel && is_array( $arrLabel ) ) {
+
+                $strModuleName = $arrLabel[0];
+            }
+
+            if ( is_string( $arrLabel ) ) {
+
+                $strModuleName = $arrLabel;
+            }
+
+            $arrReturn[$strName] = $strModuleName;
+        }
+
+        return $arrReturn;
     }
 
     public function getNavigationPlace() {

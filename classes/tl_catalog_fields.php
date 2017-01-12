4b98e0200d98d85dd3d9b8fa9801bde9e2022372
Alexander Naumov <me@alexandernaumov.de>
2017-01-12 10:58:58 +0100

    improve rgxp getter

--- classes/tl_catalog_fields.php
+++ classes/tl_catalog_fields.php
@@ -9,43 +9,52 @@ class tl_catalog_fields extends \Backend {
         return [
 
             'text',
-            'text',
             'date',
+            'radio',
             'hidden',
             'number',
-            'textarea',
             'select',
-            'radio',
-            'checkbox',
             'upload',
             'message',
+            'checkbox',
+            'textarea',
             'fieldsetStart',
             'fieldsetStop'
         ];
     }
 
-    public function getRGXPTypes() {
+    public function getRGXPTypes( \DataContainer $dc ) {
+
+        if ( $dc->activeRecord->type && $dc->activeRecord->type == 'number') {
+
+            return [ 'digit', 'natural', 'prcnt' ];
+        }
+
+        if ( $dc->activeRecord->type && $dc->activeRecord->type == 'date') {
+
+            return [ 'date', 'time', 'datim' ];
+        }
 
         return [
 
+            'url',
+            'time',
+            'date',
             'alias',
             'alnum',
             'alpha',
-            'date',
             'datim',
             'digit',
             'email',
-            'emails',
             'extnd',
-            'folderalias',
-            'friendly',
-            'language',
-            'locale',
-            'natural',
             'phone',
             'prcnt',
-            'url',
-            'time'
+            'locale',
+            'emails',
+            'natural',
+            'friendly',
+            'language',
+            'folderalias',
         ];
     }
 
@@ -60,21 +69,7 @@ class tl_catalog_fields extends \Backend {
 
     public function getSQLStatements() {
 
-        return [
-
-            'c256' => "varchar(256) NOT NULL default ''",
-
-            'c1' => "char(1) NOT NULL default ''",
-            'c16' => "varchar(16) NOT NULL default ''",
-            'c32' => "varchar(32) NOT NULL default ''",
-            'c64' => "varchar(64) NOT NULL default ''",
-            'c128' => "varchar(128) NOT NULL default ''",
-            'c512' => "varchar(512) NOT NULL default ''",
-            'c1024' => "varchar(1024) NOT NULL default ''",
-            'c2048' => "varchar(2048) NOT NULL default ''",
-            'i5' => "smallint(5) unsigned NOT NULL default '0'",
-            'i10' => "int(10) unsigned NOT NULL default '0'"
-        ];
+        return DCABuilder::$arrSQLStatements;
     }
 
     public function getCatalogFieldList( $arrRow ) {

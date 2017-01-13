c7327cf1142b0328649d76e16076c3b8cede3b63
Alexander Naumov <me@alexandernaumov.de>
2017-01-13 09:58:00 +0100

    implement sql handlers

--- library/alnv/SQLBuilder.php
+++ library/alnv/SQLBuilder.php
@@ -2,6 +2,53 @@
 
 namespace CatalogManager;
 
-class SQLBuilder {
-    
+class SQLBuilder extends \Backend {
+
+    public function parseCreateFieldStatement( $strField, $strSQLStatement ) {
+
+        return sprintf( '`%s` %s', $strField, $strSQLStatement );
+    }
+
+    public function createSQLCreateStatement( $strTable, $arrFields ) {
+
+        $strFieldStatements = [];
+
+        foreach ( $arrFields as $strField => $strSQLStatement ) {
+
+            $strFieldStatements[] = $this->parseCreateFieldStatement( $strField, $strSQLStatement );
+        }
+
+        $strFieldStatements[] = 'PRIMARY KEY  (`id`)';
+
+        $strCreateStatement = sprintf( 'CREATE TABLE `%s` ( %s ) ENGINE=MyISAM DEFAULT CHARSET=UTF8', $strTable, implode( ',', $strFieldStatements ) );
+
+        $this->Database->prepare( $strCreateStatement )->execute();
+    }
+
+    public function createSQLDropTableStatement( $strTable ) {
+
+        $strDropTableStatement = sprintf( 'DROP TABLE %s;', $strTable );
+
+        $this->Database->prepare( $strDropTableStatement )->execute();
+    }
+
+    public function alterTableField( $strTable, $strField, $strSQLStatement ) {
+
+        if ( !$this->Database->fieldExists( $strField, $strTable ) ) {
+
+            $strAlterFieldStatement = sprintf( 'ALTER TABLE %s ADD `%s` %s ', $strTable, $strField, $strSQLStatement );
+
+            $this->Database->prepare( $strAlterFieldStatement )->execute();
+        }
+    }
+
+    public function dropTableField( $strTable, $strField ) {
+
+        if ( $this->Database->fieldExists( $strField, $strTable ) ) {
+
+            $strDropFieldStatement = sprintf( 'ALTER TABLE %s DROP COLUMN `%s`', $strTable, $strField );
+
+            $this->Database->prepare( $strDropFieldStatement )->execute();
+        }
+    }
 }

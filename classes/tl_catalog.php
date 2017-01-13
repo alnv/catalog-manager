e7321fcfaacfaa1f7ff973fd3a8467fc353b2ec5
Alexander Naumov <me@alexandernaumov.de>
2017-01-13 09:57:27 +0100

    add field options

--- classes/tl_catalog.php
+++ classes/tl_catalog.php
@@ -4,7 +4,17 @@ namespace CatalogManager;
 
 class tl_catalog extends \Backend {
 
-    public function onSubmit( \DataContainer $dc ) {
+    private $arrRequiredTableFields = [
+
+        'title' => "varchar(255) NOT NULL default ''",
+        'alias' => "varchar(255) NOT NULL default ''",
+        'pid' => "int(10) unsigned NOT NULL default '0'",
+        'id' => "int(10) unsigned NOT NULL auto_increment",
+        'tstamp' => "int(10) unsigned NOT NULL default '0'",
+        'sorting' => "int(10) unsigned NOT NULL default '0'",
+    ];
+
+    public function createTableOnSubmit( \DataContainer $dc ) {
 
         if ( !$dc->id || !$dc->activeRecord->tablename ) {
 
@@ -13,13 +23,51 @@ class tl_catalog extends \Backend {
 
         if ( !$this->Database->tableExists( $dc->activeRecord->tablename ) ) {
 
-            // create new table
+            $objSQLBuilder = new SQLBuilder();
+            
+            if ( $dc->activeRecord->mode !== '4' ) {
+
+                unset( $this->arrRequiredTableFields['sorting'] );
+            }
+
+            if ( !$dc->activeRecord->pTable ) {
+
+                unset( $this->arrRequiredTableFields['pid'] );
+            }
+
+            $objSQLBuilder->createSQLCreateStatement( $dc->activeRecord->tablename, $this->arrRequiredTableFields );
+        }
+
+        else {
+
+            $objSQLBuilder = new SQLBuilder();
+
+            if ( $dc->activeRecord->mode == '4' ) {
+
+                $objSQLBuilder->alterTableField( $dc->activeRecord->tablename , 'sorting' , $this->arrRequiredTableFields['sorting'] );
+            }
+
+            else {
+
+                $objSQLBuilder->dropTableField( $dc->activeRecord->tablename , 'sorting' );
+            }
+
+            if ( $dc->activeRecord->pTable ) {
+
+                $objSQLBuilder->alterTableField( $dc->activeRecord->tablename , 'pid' , $this->arrRequiredTableFields['pid'] );
+            }
+
+            else {
+
+                $objSQLBuilder->dropTableField( $dc->activeRecord->tablename , 'pid' );
+            }
         }
     }
 
-    public function onDelete() {
+    public function dropTableOnDelete( \DataContainer $dc ) {
 
-        // delete table
+        $objSQLBuilder = new SQLBuilder();
+        $objSQLBuilder->createSQLDropTableStatement( $dc->activeRecord->tablename );
     }
 
     public function getModeTypes () {
@@ -32,14 +80,46 @@ class tl_catalog extends \Backend {
         return [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12' ];
     }
 
-    public function getDataContainerFields() {
+    public function getDataContainerFields( \DataContainer $dc ) {
 
-        return [];
+        $strID = \Input::get('id');
+        $arrDefaultFields = [ 'id', 'title', 'alias' ];
+        $objCatalogFields = $this->Database->prepare( 'SELECT * FROM tl_catalog_fields WHERE pid = ?' )->execute( $strID );
+
+        while ( $objCatalogFields->next() ) {
+
+            if ( !$objCatalogFields->fieldname ) {
+
+                continue;
+            }
+
+            $arrDefaultFields[] = $objCatalogFields->fieldname;
+        }
+
+        if ( $this->Database->fieldExists( 'sorting', $dc->activeRecord->tablename ) ) {
+
+            $arrDefaultFields[] = 'sorting';
+        }
+
+        return $arrDefaultFields;
     }
 
-    public function getParentFields() {
+    public function getAllTables( \DataContainer $dc ) {
 
-        return []; // get fields from CatalogManager
+        $arrReturn = [];
+        $objCatalogTables = $this->Database->prepare( 'SELECT `id`, `name`, `tablename` FROM tl_catalog' )->execute();
+
+        while ( $objCatalogTables->next() ) {
+
+            if ( $dc->activeRecord->tablename && $dc->activeRecord->tablename == $objCatalogTables->tablename ) {
+
+                continue;
+            }
+
+            $arrReturn[] = $objCatalogTables->tablename;
+        }
+
+        return $arrReturn;
     }
 
     public function getNavigationAreas() {

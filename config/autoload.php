5128f215172699591ac48594fa40a5a21bbc65b3
Alexander Naumov <me@alexandernaumov.de>
2017-01-15 12:57:53 +0100

    improve class loader

--- config/autoload.php
+++ config/autoload.php
@@ -1,28 +1,21 @@
 <?php
 
-if ( class_exists( 'ClassLoader' ) ) {
+ClassLoader::addNamespace( 'CatalogManager' );
 
-    ClassLoader::addNamespace( 'CatalogManager' );
-}
+ClassLoader::addClasses([
 
-if ( class_exists( 'NamespaceClassLoader' ) ) {
-
-    NamespaceClassLoader::addClassMap([
-
-        'CatalogManager\Toolkit' => 'system/modules/catalog-manager/library/alnv/Toolkit.php',
-        'CatalogManager\tl_catalog' => 'system/modules/catalog-manager/classes/tl_catalog.php',
-        'CatalogManager\Text' => 'system/modules/catalog-manager/library/alnv/fields/Text.php',
-        'CatalogManager\Date' => 'system/modules/catalog-manager/library/alnv/fields/Date.php',
-        'CatalogManager\Radio' => 'system/modules/catalog-manager/library/alnv/fields/Radio.php',
-        'CatalogManager\Select' => 'system/modules/catalog-manager/library/alnv/fields/Select.php',
-        'CatalogManager\Upload' => 'system/modules/catalog-manager/library/alnv/fields/Upload.php',
-        'CatalogManager\Number' => 'system/modules/catalog-manager/library/alnv/fields/Number.php',
-        'CatalogManager\SQLBuilder' => 'system/modules/catalog-manager/library/alnv/SQLBuilder.php',
-        'CatalogManager\DCABuilder' => 'system/modules/catalog-manager/library/alnv/DCABuilder.php',
-        'CatalogManager\Checkbox' => 'system/modules/catalog-manager/library/alnv/fields/Checkbox.php',
-        'CatalogManager\Textarea' => 'system/modules/catalog-manager/library/alnv/fields/Textarea.php',
-        'CatalogManager\CatalogManager' => 'system/modules/catalog-manager/library/alnv/CatalogManager.php',
-        'CatalogManager\tl_catalog_fields' => 'system/modules/catalog-manager/classes/tl_catalog_fields.php',
-        'CatalogManager\InitializeSystem' => 'system/modules/catalog-manager/library/alnv/InitializeSystem.php',
-    ]);
-}
+    'CatalogManager\Toolkit' => 'system/modules/catalog-manager/library/alnv/Toolkit.php',
+    'CatalogManager\tl_catalog' => 'system/modules/catalog-manager/classes/tl_catalog.php',
+    'CatalogManager\Text' => 'system/modules/catalog-manager/library/alnv/fields/Text.php',
+    'CatalogManager\Radio' => 'system/modules/catalog-manager/library/alnv/fields/Radio.php',
+    'CatalogManager\Select' => 'system/modules/catalog-manager/library/alnv/fields/Select.php',
+    'CatalogManager\Upload' => 'system/modules/catalog-manager/library/alnv/fields/Upload.php',
+    'CatalogManager\Number' => 'system/modules/catalog-manager/library/alnv/fields/Number.php',
+    'CatalogManager\SQLBuilder' => 'system/modules/catalog-manager/library/alnv/SQLBuilder.php',
+    'CatalogManager\DCABuilder' => 'system/modules/catalog-manager/library/alnv/DCABuilder.php',
+    'CatalogManager\Checkbox' => 'system/modules/catalog-manager/library/alnv/fields/Checkbox.php',
+    'CatalogManager\Textarea' => 'system/modules/catalog-manager/library/alnv/fields/Textarea.php',
+    'CatalogManager\DateInput' => 'system/modules/catalog-manager/library/alnv/fields/DateInput.php',
+    'CatalogManager\tl_catalog_fields' => 'system/modules/catalog-manager/classes/tl_catalog_fields.php',
+    'CatalogManager\InitializeCatalogManager' => 'system/modules/catalog-manager/library/alnv/InitializeCatalogManager.php'
+]);

ec2ddf6f7b9520935e7c193f400d0c134c4ec81b
Alexander Naumov <me@alexandernaumov.de>
2017-01-18 10:31:22 +0100

    add new classes

--- config/autoload.php
+++ config/autoload.php
@@ -11,11 +11,13 @@ ClassLoader::addClasses([
     'CatalogManager\Select' => 'system/modules/catalog-manager/library/alnv/fields/Select.php',
     'CatalogManager\Upload' => 'system/modules/catalog-manager/library/alnv/fields/Upload.php',
     'CatalogManager\Number' => 'system/modules/catalog-manager/library/alnv/fields/Number.php',
+    'CatalogManager\CSVBuilder' => 'system/modules/catalog-manager/library/alnv/CSVBuilder.php',
     'CatalogManager\SQLBuilder' => 'system/modules/catalog-manager/library/alnv/SQLBuilder.php',
     'CatalogManager\DCABuilder' => 'system/modules/catalog-manager/library/alnv/DCABuilder.php',
     'CatalogManager\Checkbox' => 'system/modules/catalog-manager/library/alnv/fields/Checkbox.php',
     'CatalogManager\Textarea' => 'system/modules/catalog-manager/library/alnv/fields/Textarea.php',
     'CatalogManager\DateInput' => 'system/modules/catalog-manager/library/alnv/fields/DateInput.php',
     'CatalogManager\tl_catalog_fields' => 'system/modules/catalog-manager/classes/tl_catalog_fields.php',
+    'CatalogManager\ReviseRelatedTables' => 'system/modules/catalog-manager/library/alnv/ReviseRelatedTables.php',
     'CatalogManager\InitializeCatalogManager' => 'system/modules/catalog-manager/library/alnv/InitializeCatalogManager.php'
 ]);

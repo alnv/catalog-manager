81e96085fc264fb78eed5a30e2ee59a6851be7b8
Alexander Naumov <me@alexandernaumov.de>
2017-01-20 10:18:22 +0100

    add helper functions

--- library/alnv/Toolkit.php
+++ library/alnv/Toolkit.php
@@ -18,4 +18,51 @@ class Toolkit {
 
         return [];
     }
+
+    public static function getBooleanByValue( $varValue ) {
+
+        if ( !$varValue ) {
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function deserializeAndImplode( $strValue, $strDelimiter = ',' ) {
+
+        if ( !$strValue || !is_string( $strValue ) ) {
+
+            return '';
+        }
+
+        $arrValue = deserialize( $strValue );
+
+        if ( !empty( $arrValue ) && is_array( $arrValue ) ) {
+
+            return implode( $strDelimiter, $arrValue );
+        }
+
+        return '';
+    }
+
+    public static function isDefined( $varValue ) {
+
+        if ( is_numeric( $varValue ) ) {
+
+            return true;
+        }
+
+        if ( is_array( $varValue )) {
+
+            return true;
+        }
+
+        if ( $varValue && is_string( $varValue ) ) {
+
+            return true;
+        }
+
+        return false;
+    }
 }

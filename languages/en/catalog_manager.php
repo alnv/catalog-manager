41d0ff2d8e748020824b3aaa5cdaa66996568d40
Alexander Naumov <me@alexandernaumov.de>
2017-01-23 14:33:53 +0100

    clean up labels

--- languages/en/catalog_manager.php
+++ languages/en/catalog_manager.php
@@ -6,10 +6,10 @@ $GLOBALS['TL_LANG']['catalog_manager']['legends']['general_legend'] = 'General S
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['cut'] = [ 'Move Entity', 'Move entity ID %s.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['copy'] = [ 'Copy Entity', 'Copy entity ID %s.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['edit'] = [ 'Edit Entity', 'Edit entity ID %s.' ];
-$GLOBALS['TL_LANG']['catalog_manager']['operations']['delete'] = [ 'Delete Entity', 'Delete entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['delete'] = [ 'Delete Entity', 'Delete entity ID "%s".' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['goTo'] = [ 'Edit table "%s"', 'Here you can edit table "%s".' ];
-$GLOBALS['TL_LANG']['catalog_manager']['operations']['show'] = [ 'Show Entity', 'Show the details of entity ID %s.' ];
-$GLOBALS['TL_LANG']['catalog_manager']['operations']['toggle'] = [ 'Disable Entity', 'Disable or Enable entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['show'] = [ 'Show Entity', 'Show the details of entity ID "%s".' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['toggle'] = [ 'Disable Entity', 'Disable or Enable entity ID "%s".' ];
 
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['title'] = [ 'Title', 'Please enter your entity title.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['alias'] = [ 'Entity alias', 'Please enter your entity alias.' ];

acdf07b40b858dcc2e9ba988b08252f0f6a1e0c5
Alexander Naumov <me@alexandernaumov.de>
2017-01-23 09:37:07 +0100

    implement initialize function

--- library/alnv/I18nCatalogTranslator.php
+++ library/alnv/I18nCatalogTranslator.php
@@ -4,5 +4,18 @@ namespace CatalogManager;
 
 class i18nCatalogTranslator {
 
-    // @todo
+    public function initialize() {
+
+        \Controller::loadLanguageFile( 'catalog_manager', $this->getCurrentLanguageIsoCode() );
+    }
+
+    public function getCurrentLanguageIsoCode() {
+
+        return $GLOBALS['TL_LANGUAGE'] ? $GLOBALS['TL_LANGUAGE'] : 'en';
+    }
+
+    public function getFieldLabel( $strI18nKey, $strTitle = '', $strDescription = '' ) {
+
+        return [];
+    }
 }

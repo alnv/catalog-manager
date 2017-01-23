da3df20ef64c01303092d458fba2f6f800b6521a
Alexander Naumov <me@alexandernaumov.de>
2017-01-23 10:03:05 +0100

    implement field i18n translator

--- library/alnv/I18nCatalogTranslator.php
+++ library/alnv/I18nCatalogTranslator.php
@@ -4,9 +4,13 @@ namespace CatalogManager;
 
 class i18nCatalogTranslator {
 
+    private $strConfigFile = TL_ROOT . '/' . 'system/config/i18nCatalogManager.yaml';
+
     public function initialize() {
 
         \Controller::loadLanguageFile( 'catalog_manager', $this->getCurrentLanguageIsoCode() );
+
+        $this->createI18nCatalogConfigFile();
     }
 
     public function getCurrentLanguageIsoCode() {
@@ -16,6 +20,32 @@ class i18nCatalogTranslator {
 
     public function getFieldLabel( $strI18nKey, $strTitle = '', $strDescription = '' ) {
 
+        $arrLabel = &$GLOBALS['TL_LANG']['catalog_manager']['fields'][ $strI18nKey ];
+
+        if ( empty( $arrLabel ) && !is_array( $arrLabel ) ) {
+
+            $arrLabel = [ $strTitle, $strDescription ];
+        }
+
+        // @todo yaml
+
+        return $arrLabel;
+    }
+
+    public function getYamlLanguageFile( $strType, $strI18nKey ) {
+
         return [];
+    }
+
+    private function createI18nCatalogConfigFile() {
+
+        if ( !file_exists( $this->strConfigFile ) ) {
+
+            $objFile = fopen( $this->strConfigFile, 'a' );
+
+            fwrite( $objFile, '' );
+
+            fclose( $objFile );
+        }
     }
 }

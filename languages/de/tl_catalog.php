cfdd1eed929d0c25def7bfd48c9a766414e2f117
Alexander Naumov <me@alexandernaumov.de>
2017-01-23 11:13:54 +0100

    add catalog labels

--- languages/de/tl_catalog.php
+++ languages/de/tl_catalog.php
@@ -1,1 +1,32 @@
 <?php
+
+$GLOBALS['TL_LANG']['tl_catalog']['new'] = [ 'Neues Katalog anlegen', 'Hier können Sie ein neues Katalog erstellen.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog']['label_settings'] = 'Spalten Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['table_settings'] = 'Tabellen Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['sorting_settings'] = 'Ansicht Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['panel_layout_legend'] = 'Panel Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['operations_legend'] = 'Operation Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['navigation_settings'] = 'Navigation Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_catalog']['mode'] = [ 'Ansicht', 'Bitte wählen Sie eine Ansicht aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['name'] = [ 'Modulname', 'Bitte geben Sie den Modulnamen ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['flag'] = [ 'Sortierung', 'Bitte wählen Sie eine Sortierung aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['cTables'] = [ 'Kindtabellen', 'Bitte wählen Sie Kindtabellen aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['navPosition'] = [ 'Position', 'Bitte wählen Sie eine Position aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['format'] = [ 'Format', 'Hier können Sie Ihre Spalte individualisieren.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['pTable'] = [ 'Elterntabelle', 'Bitte wählen Sie eine Elterntabelle aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['operations'] = [ 'Operationen', 'Bitte wählen Sie Ihre Operationen aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['headerFields'] = [ 'Kopffelder', 'Bitte wählen Sie Ihre Kopffelder aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['panelLayout'] = [ 'Panel', 'Hier können Sie Ihr Panel individualisieren.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['fields'] = [ 'Felder', 'Bitte wählen Sie Ihre Felder für die Ansicht aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['tablename'] = [ 'Tabelle', 'Bitte geben Sie einen validen Datenbank namen ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['description'] = [ 'Beschreibung', 'Bitte geben Sie eine kurze Beschreibung ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['navArea'] = [ 'Navigationsbereich', 'Bitte wählen Sie Ihren Navigationsbereich aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['showColumns'] = [ 'Mehrere Spalten anzeigen', 'Hier können Sie Ihre mehrere Spalten in der Ansicht anzeigen.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['isBackendModule'] = [ 'Als Back end Modul verwenden', 'Diese Tabelle soll als Back end Modul verwendet werden.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog']['delete'] = [ 'Katalog löschen', 'Katalog mit der ID %s löschen.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['edit'] = [ 'Katalog bearbeiten', 'Katalog mit der ID %s bearbeiten.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['show'] = [ 'Katalog details anzeigen', 'Die Details des Katalogs mit der ID %s anzeigen.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['editFields'] = [ 'Katalog Felder bearbeiten', 'Die Felder des Katalogs mit der ID %s bearbeiten.' ];
--- languages/en/tl_catalog.php
+++ languages/en/tl_catalog.php
@@ -1,1 +1,32 @@
 <?php
+
+$GLOBALS['TL_LANG']['tl_catalog']['new'] = [ 'Create new catalog', 'Here you can create new catalog.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog']['label_settings'] = 'Row Settings';
+$GLOBALS['TL_LANG']['tl_catalog']['table_settings'] = 'Table Settings';
+$GLOBALS['TL_LANG']['tl_catalog']['sorting_settings'] = 'View Settings';
+$GLOBALS['TL_LANG']['tl_catalog']['panel_layout_legend'] = 'Panel Settings';
+$GLOBALS['TL_LANG']['tl_catalog']['operations_legend'] = 'Operation Settings';
+$GLOBALS['TL_LANG']['tl_catalog']['navigation_settings'] = 'Navigation Settings';
+
+$GLOBALS['TL_LANG']['tl_catalog']['mode'] = [ 'View', 'Please select your view.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['flag'] = [ 'Sorting', 'Please select your sorting.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['tablename'] = [ 'Table', 'Please enter valid table name.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['name'] = [ 'Module name', 'Please enter your module name.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['cTables'] = [ 'Child tables', 'Please select your tables.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['fields'] = [ 'Fields', 'Please select fields for the view.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['navPosition'] = [ 'Position', 'Please select your position' ];
+$GLOBALS['TL_LANG']['tl_catalog']['format'] = [ 'Format', 'Here you can customize your table row.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['pTable'] = [ 'Parent table', 'Please select your parent table.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['operations'] = [ 'Operations', 'Please select your operations.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['panelLayout'] = [ 'Panel', 'Here you can customize your panel.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['navArea'] = [ 'Navigation area', 'Please select your navigation area.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['headerFields'] = [ 'Header fields', 'Please select your header fields.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['description'] = [ 'Description', 'Please enter short module description.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['showColumns'] = [ 'Multiple columns', 'Here you can show multiple columns.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['isBackendModule'] = [ 'Use as back end module', 'Here you can use this table as back end module.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog']['delete'] = [ 'Delete catalog', 'Delete catalog ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog']['edit'] = [ 'Edit catalog', 'Edit catalog ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog']['show'] = [ 'Show details', 'Show the details of catalog ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog']['editFields'] = [ 'Edit catalog fields', 'Edit the fields of catalog ID "%s".' ];

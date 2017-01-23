fa320ba80c21863c0311e38c66e86474396b964f
Alexander Naumov <me@alexandernaumov.de>
2017-01-23 09:35:44 +0100

    add catalog manager language files

---
+++ languages/en/catalog_manager.php
@@ -0,0 +1,13 @@
+<?php
+
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['cut'] = [ 'Move Entity', 'Move entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['copy'] = [ 'Copy Entity', 'Copy entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['edit'] = [ 'Edit Entity', 'Edit entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['delete'] = [ 'Delete Entity', 'Delete entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['goTo'] = [ 'Edit table "%s"', 'Here you can edit table "%s".' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['show'] = [ 'Show Entity', 'Show the details of entity ID %s.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['toggle'] = [ 'Disable Entity', 'Disable or Enable entity ID %s.' ];
+
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['title'] = [ 'Title', 'Please enter your entity title.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['alias'] = [ 'Entity alias', 'Please enter your entity alias.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['invisible'] = [ 'Hide entity', 'Here you can hide your entity.' ];

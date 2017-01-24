6dd1a3f11bea0aadd48cf77367d1376194bd3b62
Alexander Naumov <me@alexandernaumov.de>
2017-01-24 10:06:49 +0100

    implement permission handler for dca

---
+++ library/alnv/DCAPermission.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace CatalogManager;
+
+class DCAPermission extends CatalogController {
+
+    public function __construct() {
+
+        parent::__construct();
+
+        $this->import( 'Database' );
+        $this->import( 'BackendUser', 'User' );
+    }
+
+    public function checkPermission( $strTable, $strFieldname, $strFieldPermissions ) {
+
+        $strID = \Input::get( 'id' );
+        $strAct = \Input::get( 'act' );
+
+        if ( $this->User->isAdmin ) {
+
+            return null;
+        }
+
+        if ( !is_array( $this->User->{$strFieldname} ) || empty( $this->User->{$strFieldname} ) ) {
+
+            $arrRoot = [0];
+        }
+
+        else {
+
+            $arrRoot = $this->User->{$strFieldname};
+        }
+
+        $GLOBALS['TL_DCA'][$strTable]['list']['sorting']['root'] = $arrRoot;
+
+        if ( !$this->User->hasAccess( 'create', $strFieldPermissions ) ) {
+
+            $GLOBALS['TL_DCA'][$strTable]['config']['closed'] = true;
+        }
+
+        if ( !$this->User->hasAccess( 'delete', $strFieldPermissions ) ) {
+
+            unset( $GLOBALS['TL_DCA'][$strTable]['list']['operations']['copy'] );
+            unset( $GLOBALS['TL_DCA'][$strTable]['list']['operations']['delete'] );
+        }
+
+        if ( !$this->User->hasAccess( 'edit', $strFieldPermissions ) ) {
+
+            unset( $GLOBALS['TL_DCA'][$strTable]['list']['operations']['edit'] );
+        }
+
+        // @todo add hook
+
+        switch ( $strAct ) {
+
+            case 'paste':
+            case 'create':
+            case 'select':
+
+                // Allow
+
+                break;
+
+            case 'edit':
+
+                //
+
+                break;
+
+            case 'cut':
+            case 'copy':
+            case 'show':
+            case 'delete':
+            case 'toggle':
+
+                //
+
+                break;
+
+            case 'cutAll':
+            case 'copyAll':
+            case 'editAll':
+            case 'deleteAll':
+            case 'overrideAll':
+
+                //
+
+                break;
+
+            default:
+
+                if ( strlen( \Input::get('act') ) ) {
+
+                    $this->log( sprintf( 'Invalid command "%s"', $strAct ), __METHOD__, TL_ERROR);
+                    $this->redirect( 'contao/main.php?act=error' );
+                }
+
+                break;
+        }
+    }
+}

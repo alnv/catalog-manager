a5439d38dc186bb6c6521bde62a59779e7d82b8c
Alexander Naumov <me@alexandernaumov.de>
2017-01-24 10:21:32 +0100

    add catalog manager permission fields

--- dca/tl_user.php
+++ dca/tl_user.php
@@ -1,1 +1,42 @@
 <?php
+
+$GLOBALS['TL_DCA']['tl_user']['palettes']['extend'] = str_replace( 'fop;', 'fop;{catalog_manager_legend},catalog,catalogp;', $GLOBALS['TL_DCA']['tl_user']['palettes']['extend'] );
+$GLOBALS['TL_DCA']['tl_user']['palettes']['custom'] = str_replace( 'fop;', 'fop;{catalog_manager_legend},catalog,catalogp;', $GLOBALS['TL_DCA']['tl_user']['palettes']['custom'] );
+
+$GLOBALS['TL_DCA']['tl_user']['fields']['catalog'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_user']['catalog'],
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_catalog.name',
+
+    'eval' => [
+
+        'multiple' => true
+    ],
+
+    'exclude' => true,
+    'sql' => "blob NULL"
+];
+
+$GLOBALS['TL_DCA']['tl_user']['fields']['catalogp'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_user']['catalogp'],
+    'inputType' => 'checkbox',
+
+    'options' => [
+
+        'edit',
+        'create',
+        'delete'
+    ],
+
+    'eval' => [
+
+        'multiple' => true
+    ],
+
+    'reference' => &$GLOBALS['TL_LANG']['tl_user']['reference'],
+
+    'exclude' => true,
+    'sql' => "blob NULL"
+];
--- dca/tl_user_group.php
+++ dca/tl_user_group.php
@@ -1,1 +1,41 @@
 <?php
+
+$GLOBALS['TL_DCA']['tl_user_group']['palettes']['default'] = str_replace( 'fop;', 'fop;{catalog_manager_legend},catalog,catalogp;', $GLOBALS['TL_DCA']['tl_user_group']['palettes']['default'] );
+
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['catalog'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_user']['catalog'],
+    'inputType' => 'checkbox',
+    'foreignKey' => 'tl_catalog.name',
+
+    'eval' => [
+
+        'multiple' => true
+    ],
+
+    'exclude' => true,
+    'sql' => "blob NULL"
+];
+
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['catalogp'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_user']['catalogp'],
+    'inputType' => 'checkbox',
+
+    'options' => [
+
+        'edit',
+        'create',
+        'delete'
+    ],
+
+    'eval' => [
+
+        'multiple' => true
+    ],
+
+    'reference' => &$GLOBALS['TL_LANG']['tl_user']['reference'],
+
+    'exclude' => true,
+    'sql' => "blob NULL"
+];

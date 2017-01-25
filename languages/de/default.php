fd296f46bb5420ba29af7769e9d19bf0ff3132cd
Alexander Naumov <me@alexandernaumov.de>
2017-01-25 09:00:43 +0100

    add global user labels

---
+++ languages/de/default.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Löschen';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Anlegen';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Bearbeiten';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Rechte';
---
+++ languages/en/default.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Edit';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Delete';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Create';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Permissions';

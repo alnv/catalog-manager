bfa401d53ec5253fbc9865404f0b3f8abd8a5372
Alexander Naumov <me@alexandernaumov.de>
2017-01-25 16:02:57 +0100

    set up boilerplate code

--- library/alnv/Modules/ModuleUniversalView.php
+++ library/alnv/Modules/ModuleUniversalView.php
@@ -4,6 +4,12 @@ namespace CatalogManager;
 
 class ModuleUniversalView extends \Module {
 
+    private $strCatalogID;
+
+    private $arrCatalog = [];
+
+    private $arrCatalogFields =[];
+
     protected $strTemplate = 'mod_catalog_view';
 
     public function generate() {
@@ -18,7 +24,70 @@ class ModuleUniversalView extends \Module {
 
     protected function compile() {
 
-        $objCatalogView = new CatalogView();
+        $strAutoItem = \Input::get( 'auto_item' );
+
+        $this->CatalogView = new CatalogView();
+
+        $this->arrCatalog = $this->CatalogView->getCatalogByTablename( $this->catalogTablename );
+
+        $this->strCatalogID = $this->arrCatalog['id'];
+
+        $this->arrCatalogFields = $this->CatalogView->getCatalogFieldsByCatalogID( $this->strCatalogID );
+
+        if ( $strAutoItem ) {
+
+            $this->determineMasterView();
+        }
+
+        else {
+
+            $strMode = \Input::get( 'mode' . $this->id );
+
+            switch ( $strMode ) {
+
+                case 'edit':
+
+                    $this->determineEditFormView();
+
+                    break;
+
+                case 'create' :
+
+                    $this->determineCreateFormView();
+
+                    break;
+
+                default:
+
+                    $this->cdetermineCatalogView();
+
+                    break;
+            }
+        }
+    }
+
+    private function cdetermineCatalogView() {
+
+        $arrCatalogs = $this->CatalogView->getCatalogDataByTable( $this->catalogTablename, [] );
+
+        //
+    }
+
+    private function determineMasterView() {
+
+        $arrCatalogs = $this->CatalogView->getCatalogDataByTable( $this->catalogTablename, [] );
+
+        //
+    }
+
+    private function determineEditFormView() {
+
+        $arrCatalogs = $this->CatalogView->getCatalogDataByTable( $this->catalogTablename, [] );
+
+        //
+    }
+
+    private function determineCreateFormView() {
 
         //
     }

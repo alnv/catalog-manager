bfbde92c760d4d6066a11795764f89454a48dcac
Alexander Naumov <me@alexandernaumov.de>
2017-01-25 09:01:46 +0100

    clean up code

--- languages/de/catalog_manager.php
+++ languages/de/catalog_manager.php
@@ -6,11 +6,11 @@ $GLOBALS['TL_LANG']['catalog_manager']['legends']['general_legend'] = 'Allgemein
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['delete'] = [ 'Datensatz löschen', 'Datensatz mit der ID %s löschen.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['cut'] = [ 'Datensatz verschieben', 'Datensatz mit der ID %s verschieben.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['copy'] = [ 'Datensatz duplizieren', 'Datensatz mit der ID %s duplizieren.' ];
-$GLOBALS['TL_LANG']['catalog_manager']['operations']['goTo'] = [ 'Tabelle "%s" bearbeiten', 'Hier können Sie die Tabelle "%s" bearbeiten.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['edit'] = [ 'Datensatz bearbeiten', 'Die Einstellungen des Datensatzes bearbeiten.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['show'] = [ 'Details anzeigen', 'Die Details des Datensatzes mit der ID %s anzeigen.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['operations']['goTo'] = [ 'Tabelle "%s" bearbeiten', 'Hier können Sie die Tabelle "%s" bearbeiten.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['operations']['toggle'] = [ 'Datensatz ausblenden', 'Datensatz mit der ID %s veröffentlichen/ausblenden.' ];
 
-$GLOBALS['TL_LANG']['catalog_manager']['fields']['title'] = [ 'Titel', 'Bitte geben Sie Ihren Titel ein.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['alias'] = [ 'Alias', 'Bitte geben Sie ein Alias ein.' ];
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['title'] = [ 'Titel', 'Bitte geben Sie Ihren Titel ein.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['invisible'] = [ 'Verstecken', 'Hier können Sie Ihr Datensatz verstecken.' ];

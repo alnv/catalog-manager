dd3e876b958f815aef756c3206b734aff8391ff0
Alexander Naumov <me@alexandernaumov.de>
2017-01-27 09:40:36 +0100

    implement join field getter

--- classes/tl_module.php
+++ classes/tl_module.php
@@ -23,4 +23,32 @@ class tl_module extends \Backend {
 
         return $this->getTemplateGroup('catalog_');
     }
+
+    public function getJoinAbleFields( \DataContainer $dc ) {
+
+        $arrReturn = [];
+        $arrCatalog = $GLOBALS['TL_CATALOG_MANAGER']['CATALOG_EXTENSIONS'][ $dc->activeRecord->catalogTablename ];
+
+        if ( !$arrCatalog || empty( $arrCatalog ) || !is_array( $arrCatalog )  ) return $arrReturn;
+
+        $strID = $arrCatalog['id'];
+        $objCatalogFields = $this->Database->prepare( 'SELECT * FROM tl_catalog_fields WHERE pid = ?' )->execute( $strID );
+
+        while ( $objCatalogFields->next() ) {
+
+            if ( !$objCatalogFields->title ) {
+
+                continue;
+            }
+
+            if ( !$objCatalogFields->optionsType || $objCatalogFields->optionsType == 'useOptions' ) {
+
+                continue;
+            }
+
+            $arrReturn[$objCatalogFields->id] = $objCatalogFields->title;
+        }
+
+        return $arrReturn;
+    }
 }

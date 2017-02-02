eec9c8a0549503d99292c2bd375cf65225f09cf2
Alexander Naumov <me@alexandernaumov.de>
2017-02-02 10:34:49 +0100

    add member group label fields

---
+++ languages/de/tl_member_group.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_member_group']['catalog_manager_legend'] = 'Catalog Manager Berechtigungen';
+
+$GLOBALS['TL_LANG']['tl_member_group']['isAdmin'] = [ 'Zum Administrator machen', 'Administratoren haben uneingeschränkten Zugriff auf alle Kataloge!' ];
---
+++ languages/en/tl_member_group.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_member_group']['catalog_manager_legend'] = 'Catalog Manager permissions';
+
+$GLOBALS['TL_LANG']['tl_member_group']['isAdmin'] = [ 'Make the member an administrator', 'Administrators have unlimited access to all catalogs and elements!' ];

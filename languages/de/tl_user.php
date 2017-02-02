ab5bc2091b8440b348242b8e0a56af05026d4469
Alexander Naumov <me@alexandernaumov.de>
2017-02-02 10:35:02 +0100

    fix label

--- languages/de/tl_user.php
+++ languages/de/tl_user.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_user']['catalog_manager_legend'] = 'Catalog Manager permissions';
+$GLOBALS['TL_LANG']['tl_user']['catalog_manager_legend'] = 'Catalog Manager Berechtigungen';
 
 $GLOBALS['TL_LANG']['tl_user']['catalog'] = [ 'Catalog Manager', 'Hier können Sie den Zugriff auf eine oder mehrere Kataloge erlauben.' ];
 $GLOBALS['TL_LANG']['tl_user']['catalogp'] = [ 'Catalog Manager permissions', 'Hier können Sie die Katalog Rechte festlegen.' ];

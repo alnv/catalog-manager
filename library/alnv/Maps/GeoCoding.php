d66d7bc2ee3249f08a7d314c64979f492d7d35c3
Alexander Naumov <me@alexandernaumov.de>
2017-02-08 12:28:16 +0100

    implement google maps geo coding class

---
+++ library/alnv/Maps/GeoCoding.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace CatalogManager;
+
+class GeoCoding extends CatalogController {
+
+    private $strCity;
+    private $strStreet;
+    private $strPostal;
+    private $strCountry;
+
+    private $arrGoogleMapsCache = [];
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+    public function getCords( $strAddress = '', $strLanguage = 'en', $blnServer = false ) {
+
+        $arrReturn = [
+
+            'lat' => '',
+            'lng' => ''
+        ];
+
+        if ( !$strAddress ) {
+
+            $strAddress = sprintf( '%s %s %s %s', $this->strStreet, $this->strPostal, $this->strCity, $this->strCountry );
+        }
+
+        $strGoogleIDKey = '';
+        $strCacheID = md5( urlencode( $strAddress ) );
+        $strGoogleID = $blnServer ? \Config::get('catalogGoogleMapsServerKey') : \Config::get('catalogGoogleMapsClientKey');
+
+        if( is_array( $this->arrGoogleMapsCache[ $strCacheID ] ) ) {
+
+            return $this->arrGoogleMapsCache[ $strCacheID ];
+        }
+
+        if ( $strGoogleID ) {
+
+            $strGoogleIDKey = sprintf( '&key=%s', $strGoogleID );
+        }
+
+        $strGoogleMapsRequest = sprintf( 'https://maps.googleapis.com/maps/api/geocode/json?address=%s%s&language=%s&region=%s' , urlencode( $strAddress ),$strGoogleIDKey, urlencode( $strLanguage ), strlen( $strLanguage ) );
+
+        $objRequest = new \Request();
+        $objRequest->send( $strGoogleMapsRequest );
+
+        if ( $objRequest->hasError() ) {
+
+            return $arrReturn;
+        }
+
+        $objResponse = $objRequest->response ? json_decode( $objRequest->response, true ) : [];
+
+        if( !empty( $objResponse ) && is_array( $objResponse ) ) {
+
+            $arrGeometry = $objResponse['results'][0]['geometry'];
+
+            $arrReturn['lat'] = $arrGeometry['location'] ? $arrGeometry['location']['lat'] : '';
+            $arrReturn['lng'] = $arrGeometry['location'] ? $arrGeometry['location']['lng'] : '';
+
+            $this->arrGoogleMapsCache[ $strCacheID ] = $arrReturn;
+
+            return $arrReturn;
+        }
+
+        return $arrReturn;
+    }
+
+    public function setCity( $strCity ) {
+
+        $this->strCity = $strCity;
+    }
+
+    public function setStreet( $strCity ) {
+
+        $this->strStreet = $strCity;
+    }
+
+    public function setPostal( $strCity ) {
+
+        $this->strPostal = $strCity;
+    }
+
+    public function setCountry( $strCity ) {
+
+        $this->strCountry = $strCity;
+    }
+}

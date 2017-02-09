990b132e018278936502725052ae39bc0b34a686
Alexander Naumov <me@alexandernaumov.de>
2017-02-09 09:20:58 +0100

    add map class

---
+++ library/alnv/Fields/Map.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CatalogManager;
+
+class Map {
+
+    public static function generate( $arrDCAField, $arrField ) {}
+
+    public static function parseValue( $varValue, $arrField, $arrCatalog = [] ) {
+
+        return $varValue;
+    }
+}

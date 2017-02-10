17c2bba5f228cb8734e5a5991a0c15453bef1b16
Alexander Naumov <me@alexandernaumov.de>
2017-02-10 15:05:31 +0100

    inherit generate function from text

--- library/alnv/Fields/Hidden.php
+++ library/alnv/Fields/Hidden.php
@@ -4,16 +4,9 @@ namespace CatalogManager;
 
 class Hidden {
 
-    
+
     public static function generate( $arrDCAField, $arrField ) {
 
-        $arrDCAField['eval']['hideInput'] = true;
-
-        if ( $arrField['tstampAsDefault'] ) {
-
-            $arrDCAField['default'] = time();
-        }
-
-        return $arrDCAField;
+        return Text::generate( $arrDCAField, $arrField );
     }
 }

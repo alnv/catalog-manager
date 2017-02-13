0c49ae9f5c145612fe4b58cd553fdf8cffe7d533
Alexander Naumov <me@alexandernaumov.de>
2017-02-13 13:10:57 +0100

    add widget labels

--- languages/de/default.php
+++ languages/de/default.php
@@ -4,3 +4,5 @@ $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Löschen';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Anlegen';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Bearbeiten';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Rechte';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerFields'] = 'Felder';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerOrder'] = 'Sortierreihenfolge';
--- languages/en/default.php
+++ languages/en/default.php
@@ -4,3 +4,5 @@ $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Edit';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Delete';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Create';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Permissions';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerFields'] = 'Fields';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerOrder'] ='Sort sequence';

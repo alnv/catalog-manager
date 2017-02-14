7fe92ea11079233a4ae976fc6a0dc25c12993ea6
Alexander Naumov <me@alexandernaumov.de>
2017-02-14 11:18:41 +0100

    add new widget

--- config/config.php
+++ config/config.php
@@ -44,3 +44,4 @@ $GLOBALS['TL_PERMISSIONS'][] = 'catalog';
 $GLOBALS['TL_PERMISSIONS'][] = 'catalogp';
 
 $GLOBALS['BE_FFL']['catalogOrderByWizard'] = 'CatalogManager\CatalogOrderByWizard';
+$GLOBALS['BE_FFL']['catalogInPageRelationWizard'] = 'CatalogManager\CatalogInPageRelationWizard';

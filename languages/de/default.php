9304b41a56634ceeeda4a02023459b5a8dc22c0b
Alexander Naumov <me@alexandernaumov.de>
2017-02-14 11:18:50 +0100

    add labels

--- languages/de/default.php
+++ languages/de/default.php
@@ -1,5 +1,6 @@
 <?php
 
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['table'] = 'Tabelle';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Löschen';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Anlegen';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['asc'] = 'Aufsteigend';
@@ -7,4 +8,5 @@ $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['desc'] = 'Absteigend';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Bearbeiten';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Rechte';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerFields'] = 'Felder';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogView'] = 'Katalog Einstellungen';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerOrder'] = 'Sortierreihenfolge';
--- languages/en/default.php
+++ languages/en/default.php
@@ -1,10 +1,12 @@
 <?php
 
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Edit';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['table'] = 'Table';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Delete';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Create';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['asc'] = 'Ascending';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['desc'] = 'Descending';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Permissions';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerFields'] = 'Fields';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogView'] = 'Catalog Settings';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerOrder'] ='Sort sequence';

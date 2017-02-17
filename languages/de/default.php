bee5fa55cc1693c81dffd3f98a56a76f894c48de
Alexander Naumov <me@alexandernaumov.de>
2017-02-17 09:17:10 +0100

    add taxonomy wizard labels

--- languages/de/default.php
+++ languages/de/default.php
@@ -1,6 +1,8 @@
 <?php
 
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['pageID'] = 'URL';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['field'] = 'Feld';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['value'] = 'Wert';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['table'] = 'Tabelle';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Löschen';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Anlegen';
@@ -8,5 +10,6 @@ $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['asc'] = 'Aufsteigend';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['desc'] = 'Absteigend';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Bearbeiten';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Rechte';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['operator'] = 'Operator';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerFields'] = 'Felder';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerOrder'] = 'Sortierreihenfolge';
--- languages/en/default.php
+++ languages/en/default.php
@@ -2,11 +2,14 @@
 
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['edit'] = 'Edit';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['pageID'] = 'URL';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['field'] = 'Field';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['value'] = 'Value';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['table'] = 'Table';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['delete'] = 'Delete';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['create'] = 'Create';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['asc'] = 'Ascending';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['desc'] = 'Descending';
+$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['operator'] = 'Operator';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['permission'] = 'Permissions';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerFields'] = 'Fields';
 $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['catalogManagerOrder'] ='Sort sequence';

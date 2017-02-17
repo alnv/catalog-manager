63c1e67fb6e1b79e9140206423094da771082988
Alexander Naumov <me@alexandernaumov.de>
2017-02-17 15:41:39 +0100

    rename class

---
+++ library/alnv/Fields/MessageInput.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CatalogManager;
+
+class MessageInput {
+
+    public static function generate( $arrDCAField, $arrField ) {
+
+        unset( $arrDCAField['label'][1] );
+        unset( $arrDCAField['exclude'] );
+        unset( $arrDCAField['sorting'] );
+        unset( $arrDCAField['filter'] );
+        unset( $arrDCAField['search'] );
+        unset( $arrDCAField['sql'] );
+
+        $arrDCAField['eval']['ctlgMessage'] = $arrField['message'] ? $arrField['message'] : '';
+
+        return $arrDCAField;
+    }
+
+    public static function parseValue( $varValue, $arrField, $arrCatalog = [] ) {
+
+        return '';
+    }
+}

d28b3e270ec572680d9baccdb37415eed8f7601d
Alexander Naumov <me@alexandernaumov.de>
2017-02-20 12:26:35 +0100

    add class

---
+++ library/alnv/Inserttags/FilterValueInsertTag.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CatalogManager;
+
+class FilterValueInsertTag extends \Frontend {
+
+
+    public function getInsertTagValue( $strTag ) {
+
+        $arrTags = explode('::', $strTag);
+
+        if ( is_array( $arrTags ) && $arrTags[0] == 'CTLG_FILTER_VALUE' && isset( $arrTags[1] ) ) {
+
+            $strDefaultValue = $arrTags[2] ? $arrTags[2] : '';
+
+            return \Input::get( $arrTags[1] ) ? \Input::get( $arrTags[1] ) : $strDefaultValue;
+        }
+
+        return '';
+    }
+}
---
+++ library/alnv/Widgets/CatalogFilterFieldTemplateWizard.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CatalogManager;
+
+class CatalogFilterFieldTemplateWizard extends \Widget {
+
+
+    protected $strTemplate = 'be_widget';
+
+
+    public function validate() {
+
+        parent::validate();
+    }
+
+
+    public function generate() {
+
+        return '-';
+    }
+}

cdbf8ff6b944e1a7dd2774fa912162881c00e90c
Alexander Naumov <me@alexandernaumov.de>
2017-02-20 11:37:05 +0100

    implement form fields parser

--- library/alnv/CatalogFilter.php
+++ library/alnv/CatalogFilter.php
@@ -7,12 +7,14 @@ class CatalogFilter extends CatalogController {
 
     public $strTable;
     public $arrFields = [];
+    public $arrCatalog = [];
     public $arrOptions = [];
     public $arrActiveFields = [];
 
     private $arrForbiddenFilterTypes = [
 
         'map',
+        'upload',
         'message',
         'fieldsetStop',
         'fieldsetStart'
@@ -24,14 +26,47 @@ class CatalogFilter extends CatalogController {
         parent::__construct();
 
         $this->import( 'Database' );
+        $this->import( 'DCABuilderHelper' );
     }
 
 
     public function initialize() {
 
         $this->setOptions();
+        $this->setCatalog();
         $this->getFilterFields();
         $this->setActiveFields();
+    }
+
+
+    public function generateForm() {
+
+        $intIndex = 0;
+        $strFields = '';
+
+        if ( !empty( $this->arrActiveFields ) && is_array( $this->arrActiveFields ) ) {
+
+            $arrDCFields = $this->DCABuilderHelper->convertCatalogFields2DCA( $this->arrActiveFields, [], $this->arrCatalog );
+
+            foreach ( $arrDCFields as $arrField ) {
+
+                $arrField = $this->convertWidgetToField( $arrField );
+                $strClass = $this->fieldClassExist( $arrField['inputType'] );
+
+                if ( $strClass == false ) return null;
+
+                $objWidget = new $strClass( $strClass::getAttributesFromDca( $arrField, $arrField['_fieldname'], $arrField['default'], '', '' ) );
+
+                $objWidget->id = 'id_' . $arrField['_fieldname'];
+                $objWidget->value = \Input::get( $arrField['_fieldname'] ) ? \Input::get( $arrField['_fieldname'] ) : '';
+                $objWidget->rowClass = 'row_' . $intIndex . ( ( $intIndex == 0 ) ? ' row_first' : '' ) . ( ( ( $intIndex % 2 ) == 0 ) ? ' even' : ' odd' );
+
+                $strFields .= $objWidget->parse();
+                $intIndex++;
+            }
+        }
+
+        return $strFields;
     }
 
 
@@ -79,5 +114,43 @@ class CatalogFilter extends CatalogController {
                 $this->arrActiveFields[ $strFieldID ] = $this->arrFields[ $strFieldID ];
             }
         }
+    }
+
+
+    protected function setCatalog() {
+
+        $this->arrCatalog = $this->Database->prepare('SELECT * FROM tl_catalog WHERE tablename = ?')->limit(1)->execute( $this->strTable )->row();
+    }
+
+
+    protected function convertWidgetToField( $arrField ) {
+
+        if ( $arrField['inputType'] == 'checkboxWizard' ) {
+
+            $arrField['inputType'] = 'checkbox';
+        }
+
+        if ( $arrField['inputType'] == 'checkboxWizard' ) {
+
+            $arrField['inputType'] = 'checkbox';
+        }
+
+        $arrField['eval']['tableless'] = '1';
+        $arrField['eval']['required'] = false;
+
+        return $arrField;
+    }
+
+
+    protected function fieldClassExist( $strInputType ) {
+
+        $strClass = $GLOBALS['TL_FFL'][ $strInputType ];
+
+        if ( !class_exists( $strClass ) ) {
+
+            return false;
+        }
+
+        return $strClass;
     }
 }

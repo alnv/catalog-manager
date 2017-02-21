236918b8ee2d25fabf51fd7452892484a0c52991
Alexander Naumov <me@alexandernaumov.de>
2017-02-21 11:25:02 +0100

    extend global templates variables

--- library/alnv/Modules/ModuleCatalogFilter.php
+++ library/alnv/Modules/ModuleCatalogFilter.php
@@ -31,7 +31,9 @@ class ModuleCatalogFilter extends \Module {
         $this->CatalogFilter->strTable = $this->catalogTablename;
         $this->CatalogFilter->initialize();
 
+        $this->Template->reset = $this->CatalogFilter->setResetLink();
         $this->Template->output = $this->CatalogFilter->generateForm();
-        $this->Template->action = \Environment::get( 'indexFreeRequest' );
+        $this->Template->action = $this->CatalogFilter->setActionAttribute();
+        $this->Template->disableSubmit = $this->catalogDisableSubmit ? true : false;
     }
 }

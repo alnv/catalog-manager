c82765274ade4b3a4ff8752b5c69ec1dccc98287
Alexander Naumov <me@alexandernaumov.de>
2017-02-24 09:20:12 +0100

    add parent columns getter function

--- classes/tl_catalog.php
+++ classes/tl_catalog.php
@@ -381,4 +381,21 @@ class tl_catalog extends \Backend {
 
         return [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ];
     }
+
+
+    public function getParentColumns( \DataContainer $dc ) {
+
+        if ( !$dc->activeRecord->pTable ) {
+
+            return [];
+        }
+
+        if ( $dc->activeRecord->pTable && $this->Database->tableExists( $dc->activeRecord->pTable ) ) {
+
+            $arrColumns = $this->Database->listFields( $dc->activeRecord->pTable );
+            $arrColumns = Toolkit::parseColumns( $arrColumns );
+        }
+
+        return $arrColumns;
+    }
 }

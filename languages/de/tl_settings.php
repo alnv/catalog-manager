19f408e667db87f5aae58c40b1537ca9e393c969
Alexander Naumov <me@alexandernaumov.de>
2017-03-06 14:08:12 +0100

    add catalog manager settings labels

---
+++ languages/de/tl_settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['catalog_manager_google_maps_settings'] = 'Catalog Google-Maps Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'] = [ 'Google-Maps Server Key', 'Bitte geben Sie den Google-Maps Server Key ein.' ];
+$GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'] = [ 'Google-Maps Client Key', 'Bitte geben Sie den Google-Maps Client Key ein.' ];
---
+++ languages/en/tl_settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['catalog_manager_google_maps_settings'] = 'Catalog Google-Maps Settings';
+
+$GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'] = [ 'Google-Maps Server Key', 'Please enter a google maps server key.' ];
+$GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'] = [ 'Google-Maps Client Key', 'Please enter a google maps client key.' ];

baa9a6793204d74a6b9ddf66b857c0d5693f4429
Alexander Naumov <me@alexandernaumov.de>
2017-03-06 14:08:25 +0100

    add fe module labels

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -1,3 +1,7 @@
 <?php
 
 $GLOBALS['TL_LANG']['MOD']['catalog-manager'] = [ 'Catalog Manager', 'Hier können Sie Ihre Kataloge verwalten.' ];
+
+$GLOBALS['TL_LANG']['FMD']['catalogMasterView'] = [ 'Catalog Detailansicht', 'Hier können Sie eine Detailansicht erstellen.' ];
+$GLOBALS['TL_LANG']['FMD']['catalogFilter'] = [ 'Catalog Filterformular', 'Hier können Sie ein Filterformular für die Listenansicht erstellen.' ];
+$GLOBALS['TL_LANG']['FMD']['catalogUniversalView'] = [ 'Catalog Listenansicht', 'Hier können Sie eine Listenansicht für Ihre Catalog Datensätze erstellen.' ];
--- languages/en/modules.php
+++ languages/en/modules.php
@@ -1,3 +1,7 @@
 <?php
 
 $GLOBALS['TL_LANG']['MOD']['catalog-manager'] = [ 'Catalog Manager', 'Here you can manage your catalogs.' ];
+
+$GLOBALS['TL_LANG']['FMD']['catalogMasterView'] = [ 'Catalog master view', 'Here you can create a master view.' ];
+$GLOBALS['TL_LANG']['FMD']['catalogFilter'] = [ 'Catalog filter form', 'Here you can create a filter form for the catalog list view.' ];
+$GLOBALS['TL_LANG']['FMD']['catalogUniversalView'] = [ 'Catalog list view', 'Here you can create a list view for the catalog entities.' ];

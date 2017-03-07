a9f9838fa8e6880dce3a2bcbf1bc4da551b7be59
Alexander Naumov <me@alexandernaumov.de>
2017-03-07 09:19:16 +0100

    add icon getter

--- library/alnv/IconGetter.php
+++ library/alnv/IconGetter.php
@@ -13,21 +13,33 @@ class IconGetter extends CatalogController {
     public function setCatalogIcon( $strTablename ) {
 
         $strIconname = $strTablename . '-' . 'icon';
+        $strCustomIcon = $this->getIcon( $strIconname );
 
-        if ( $this->iconExist( $strIconname, 'gif' ) ) {
-
-            return $this->strDirectory . '/' . $strIconname . '.gif';
-        }
-
-        foreach ( $this->arrFileFormats as $strFileFormat ) {
-
-            if ( $this->iconExist( $strIconname, $strFileFormat ) ) {
-
-                return $this->strDirectory . '/' . $strIconname . '.' . $strFileFormat;
-            }
-        }
+        if ( $strCustomIcon != '' ) return $strCustomIcon;
 
         return 'system/modules/catalog-manager/assets/icons/catalog-icon.gif';
+    }
+
+
+    public function setTreeViewIcon( $strTablename, $arrRow, $strLabel, \DataContainer $dc = null, $strImageAttribute = '', $blnReturnImage = false, $blnProtected = false ) {
+
+        $strIconname = $strTablename . '_' . 'tag.gif';
+        $strCustomIcon = $this->getIcon( $strIconname );
+        $strIcon = 'system/modules/catalog-manager/assets/icons/tag-icon.gif';
+
+        if ( $strCustomIcon != '' ) {
+
+            $strIcon = $strCustomIcon;
+        }
+
+        $strImageAttribute = trim( $strImageAttribute . ' data-icon="edit.gif" data-icon-disabled="header.gif" ');
+
+        if ( $arrRow['pid'] == '0' ) {
+
+            $strLabel = '<strong>' . $strLabel . '</strong>';
+        }
+
+        return \Image::getHtml( $strIcon, '', $strImageAttribute ) . ' <span>' . $strLabel . '</span>';
     }
 
 
@@ -50,5 +62,24 @@ class IconGetter extends CatalogController {
         }
 
         return false;
+    }
+
+
+    protected function getIcon( $strIconname ) {
+
+        if ( $this->iconExist( $strIconname, 'gif' ) ) {
+
+            return $this->strDirectory . '/' . $strIconname . '.gif';
+        }
+
+        foreach ( $this->arrFileFormats as $strFileFormat ) {
+
+            if ( $this->iconExist( $strIconname, $strFileFormat ) ) {
+
+                return $this->strDirectory . '/' . $strIconname . '.' . $strFileFormat;
+            }
+        }
+
+        return '';
     }
 }

03aa1965fd85469f8cffd1dc3324dae80660b541
Alexander Naumov <me@alexandernaumov.de>
2017-03-14 14:24:15 +0100

    add fe catalog manager label

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -2,6 +2,7 @@
 
 $GLOBALS['TL_LANG']['MOD']['catalog-manager'] = [ 'Catalog Manager', 'Hier können Sie Ihre Kataloge verwalten.' ];
 
+$GLOBALS['TL_LANG']['FMD']['catalog-manager'] = ['Catalog Manager', 'Alle Frontend Module des Catalog Managers'];
 $GLOBALS['TL_LANG']['FMD']['catalogMasterView'] = [ 'Catalog Detailansicht', 'Hier können Sie eine Detailansicht erstellen.' ];
 $GLOBALS['TL_LANG']['FMD']['catalogFilter'] = [ 'Catalog Filterformular', 'Hier können Sie ein Filterformular für die Listenansicht erstellen.' ];
 $GLOBALS['TL_LANG']['FMD']['catalogUniversalView'] = [ 'Catalog Listenansicht', 'Hier können Sie eine Listenansicht für Ihre Catalog Datensätze erstellen.' ];
--- languages/en/modules.php
+++ languages/en/modules.php
@@ -2,6 +2,7 @@
 
 $GLOBALS['TL_LANG']['MOD']['catalog-manager'] = [ 'Catalog Manager', 'Here you can manage your catalogs.' ];
 
+$GLOBALS['TL_LANG']['FMD']['catalog-manager'] = ['Catalog Manager', 'All frontend modules of Catalog Manager'];
 $GLOBALS['TL_LANG']['FMD']['catalogMasterView'] = [ 'Catalog master view', 'Here you can create a master view.' ];
 $GLOBALS['TL_LANG']['FMD']['catalogFilter'] = [ 'Catalog filter form', 'Here you can create a filter form for the catalog list view.' ];
 $GLOBALS['TL_LANG']['FMD']['catalogUniversalView'] = [ 'Catalog list view', 'Here you can create a list view for the catalog entities.' ];

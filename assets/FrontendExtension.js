2f70e5532aa6f3b1bf47e08387e4068d5976e01a
Alexander Naumov <me@alexandernaumov.de>
2017-03-14 11:49:00 +0100

    add empty js file for front end

---
+++ assets/FrontendExtension.js
@@ -0,0 +1,15 @@
+(function () {
+
+    'use strict';
+
+    if ( typeof window.addEventListener != "undefined" ) {
+
+        window.addEventListener( 'DOMContentLoaded', initialize, false );
+    }
+
+    function initialize() {
+
+        // @todo
+    }
+
+})();

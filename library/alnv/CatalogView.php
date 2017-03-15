69dcc835e800477f56be03f64d2448fb639dc6b3
Alexander Naumov <me@alexandernaumov.de>
2017-03-15 10:16:02 +0100

    remove auto focus handler

--- library/alnv/CatalogView.php
+++ library/alnv/CatalogView.php
@@ -71,16 +71,7 @@ class CatalogView extends CatalogController {
         }
 
         if ( $this->catalogUseMap && $this->strMode == 'view' ) {
-
-            /*
-            if ( $this->catalogRadioSearchZoomFactor && $this->catalogUseRadiusSearch ) {
-
-                $intDistance = \Input::get( 'rs_dstnc' ) ? intval( \Input::get( 'rs_dstnc' ) ) : 0;
-                
-                if ( $intDistance ) $this->catalogMapZoom = Toolkit::getZoomFactor( $intDistance );
-            }
-            */
-
+            
             $this->arrCatalogMapViewOptions = Map::getMapViewOptions([
 
                 'id' => 'map_' . $this->id,

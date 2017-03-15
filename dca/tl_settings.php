87d2402a9eec1fae4c6dd4d0b268e2c073594d05
Alexander Naumov <me@alexandernaumov.de>
2017-03-15 12:53:34 +0100

    add licence field

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -1,11 +1,12 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace( 'sslProxyDomain;', 'sslProxyDomain;{catalog_manager_google_maps_settings},catalogGoogleMapsServerKey,catalogGoogleMapsClientKey;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] );
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace( 'sslProxyDomain;', 'sslProxyDomain;{catalog_manager_settings},catalogLicence,catalogGoogleMapsServerKey,catalogGoogleMapsClientKey;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] );
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['catalogGoogleMapsServerKey'] = [
 
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'],
     'inputType' => 'text',
+
     'eval' => [
 
         'tl_class' => 'w50'
@@ -16,8 +17,20 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['catalogGoogleMapsClientKey'] = [
 
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'],
     'inputType' => 'text',
+
     'eval' => [
 
         'tl_class' => 'w50'
+    ]
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['catalogLicence'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['catalogLicence'],
+    'inputType' => 'text',
+
+    'eval' => [
+
+        'tl_class' => 'w50 long'
     ]
 ];

a30f3366ef45b0b580b84c204ae9c4be23fee111
Alexander Naumov <me@alexandernaumov.de>
2017-03-15 12:53:10 +0100

    add label

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -1,6 +1,7 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_settings']['catalog_manager_google_maps_settings'] = 'Catalog Google-Maps Einstellungen';
+$GLOBALS['TL_LANG']['tl_settings']['catalog_manager_settings'] = 'Catalog Manager Einstellungen';
 
+    $GLOBALS['TL_LANG']['tl_settings']['catalogLicence'] = [ 'Catalog Manager Lizenz', 'Bitte geben Sie Ihre Lizenz ein.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'] = [ 'Google-Maps Server Key', 'Bitte geben Sie den Google-Maps Server Key ein.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'] = [ 'Google-Maps Client Key', 'Bitte geben Sie den Google-Maps Client Key ein.' ];
--- languages/en/tl_settings.php
+++ languages/en/tl_settings.php
@@ -1,6 +1,7 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_settings']['catalog_manager_google_maps_settings'] = 'Catalog Google-Maps Settings';
+$GLOBALS['TL_LANG']['tl_settings']['catalog_manager_settings'] = 'Catalog Manager Settings';
 
+$GLOBALS['TL_LANG']['tl_settings']['catalogLicence'] = [ 'Catalog Manager licence', 'Please enter your Catalog Manager licence.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'] = [ 'Google-Maps Server Key', 'Please enter a google maps server key.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'] = [ 'Google-Maps Client Key', 'Please enter a google maps client key.' ];

e9a34aec264865a1ac1ec91f89557f030935644c
Alexander Naumov <me@alexandernaumov.de>
2017-03-16 15:28:22 +0100

    improve licence verification handler

--- classes/tl_settings.php
+++ classes/tl_settings.php
@@ -7,11 +7,11 @@ class tl_settings extends \Backend {
 
     public function verifyLicence( $varValue ) {
 
-        if ( !$varValue ) return '';
-
         $objCatalogManagerVerification = new CatalogManagerVerification();
         $blnValidLicence = $objCatalogManagerVerification->verify( $varValue, false );
-        
+
+        if ( !$varValue ) return '';
+
         if ( !$blnValidLicence ) {
 
             throw new \Exception( $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['invalidKey'] );

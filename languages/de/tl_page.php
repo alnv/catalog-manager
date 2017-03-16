c6106cd088fb30d40fd85e1aba1fc6453d359d16
Alexander Naumov <me@alexandernaumov.de>
2017-03-16 12:27:08 +0100

    add labels

---
+++ languages/de/tl_page.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_page']['catalogSettings_legend'] = 'Catalog Manager Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_page']['catalogMasterTable'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogChangeLanguageTable'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogUseMaster'] = [ 'Master verwenden', 'Hier können Sie einen Master-Datensatz für Ihre Taxonomy-Einstellungen auswählen.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogUseChangeLanguage'] = [ 'Einträge übersetzen', 'Diese Einstellung erlaubt es Ihnen die Einträge auf einer Detailseite via Sprachwechsler Modul zu wechseln.' ];
---
+++ languages/en/tl_page.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_page']['catalogSettings_legend'] = 'Catalog Manager Settings';
+
+$GLOBALS['TL_LANG']['tl_page']['catalogMasterTable'] = [ 'Table', 'Please select a Table.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogChangeLanguageTable'] = [ 'Table', 'Please select a Table.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogUseMaster'] = [ 'Use Master', 'Here you can select a master table for your taxonomies settings.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogUseChangeLanguage'] = [ 'Translate items', 'This allows you to change the entries on a detail page via changelanguage module.' ];

63fc3ace1e6a3389ee834045fcb4f6652802516b
Alexander Naumov <me@alexandernaumov.de>
2017-04-05 10:56:52 +0200

    add missing labels

--- languages/en/catalog_manager.php
+++ languages/en/catalog_manager.php
@@ -20,3 +20,8 @@ $GLOBALS['TL_LANG']['catalog_manager']['fields']['alias'] = [ 'Entity alias', 'P
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['invisible'] = [ 'Hide entity', 'Here you can hide your entity.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['start'] = [ 'Show from', 'Do not show the entity on the website before this day.' ];
 $GLOBALS['TL_LANG']['catalog_manager']['fields']['stop'] = [ 'Show until', 'Do not show the entity on the website on and after this day.' ];
+
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['id'] = [ 'ID', '' ];
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['pid'] = [ 'Parent ID', '' ];
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['sorting'] = [ 'Sorting value', '' ];
+$GLOBALS['TL_LANG']['catalog_manager']['fields']['tstamp'] = [ 'Revision date', '' ];

e84fa9020eb876b535a232ab0258e5e3584d4814
Alexander Naumov <me@alexandernaumov.de>
2017-04-20 10:10:50 +0200

    improve backend template

--- library/alnv/Modules/ModuleUniversalView.php
+++ library/alnv/Modules/ModuleUniversalView.php
@@ -15,8 +15,12 @@ class ModuleUniversalView extends \Module {
         if ( TL_MODE == 'BE' ) {
 
             $objTemplate = new \BackendTemplate('be_wildcard');
-            $objTemplate->wildcard = '### ' . $this->name . ' ###';
+
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->title = $this->headline;
             $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+            $objTemplate->wildcard = '### ' . utf8_strtoupper( $GLOBALS['TL_LANG']['FMD']['catalogUniversalView'][0] ) . ' ###';
 
             return $objTemplate->parse();
         }

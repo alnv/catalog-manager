e9c8a835a27377d0b2c17f859e68417b3c49853c
Alexander Naumov <me@alexandernaumov.de>
2017-05-02 17:41:38 +0200

    remove field

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -6,9 +6,9 @@ $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'catalogUseChangeL
 
 $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace( 'includeLayout;', 'includeLayout;{catalogSettings_legend},catalogUseMaster,catalogUseChangeLanguage,catalogUseRouting;', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] );
 
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseRouting'] = 'catalogRouting';
 $GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseMaster'] = 'catalogMasterTable';
 $GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseChangeLanguage'] = 'catalogChangeLanguageTable';
-$GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseRouting'] = 'catalogRouting,catalogDetermineRoutingTable';
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['catalogUseMaster'] = [
 
@@ -67,27 +67,6 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['catalogRouting'] = [
         'mandatory' => true,
         'doNotCopy' => true,
     ],
-
-    'exclude' => true,
-    'sql' => "varchar(128) NOT NULL default ''"
-];
-
-$GLOBALS['TL_DCA']['tl_page']['fields']['catalogDetermineRoutingTable'] = [
-
-    'label' => &$GLOBALS['TL_LANG']['tl_page']['catalogDetermineRoutingTable'],
-    'inputType' => 'select',
-
-    'eval' => [
-
-        'chosen' => true,
-        'maxlength' => 128,
-        'tl_class' => 'clr',
-        'doNotCopy' => true,
-        'blankOptionLabel' => '-',
-        'includeBlankOption' => true
-    ],
-
-    'options_callback' => [ 'CatalogManager\tl_page', 'getCatalogTables' ],
 
     'exclude' => true,
     'sql' => "varchar(128) NOT NULL default ''"

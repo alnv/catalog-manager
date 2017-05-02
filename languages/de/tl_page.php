48fd549c30a2e08d1878d100a0a21dcfb4b83e36
Alexander Naumov <me@alexandernaumov.de>
2017-05-02 15:59:57 +0200

    add field labls

--- languages/de/tl_page.php
+++ languages/de/tl_page.php
@@ -4,5 +4,8 @@ $GLOBALS['TL_LANG']['tl_page']['catalogSettings_legend'] = 'Catalog Manager-Eins
 
 $GLOBALS['TL_LANG']['tl_page']['catalogMasterTable'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogChangeLanguageTable'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogUseRouting'] = [ 'Routing verwenden', 'Hier können das Routing verhalten überschreiben.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogRouting'] = [ 'Routing', 'Hier können ein neues Routing Muster eintragen. Zum Beispiel {myFieldname}/{auto_item}.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogUseMaster'] = [ 'Master verwenden', 'Hier können Sie einen Master-Datensatz für Ihre Taxonomy-Einstellungen auswählen.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogDetermineRoutingTable'] = [ 'Weiterleitungslinks einschränken.', 'Saubere Weiterleitungslinks nur für eine bestimmten Tabelle verwenden.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogUseChangeLanguage'] = [ 'Einträge übersetzen', 'Diese Einstellung erlaubt es Ihnen die Einträge auf einer Detailseite via Sprachwechsler Modul zu wechseln.' ];
--- languages/en/tl_page.php
+++ languages/en/tl_page.php
@@ -4,5 +4,8 @@ $GLOBALS['TL_LANG']['tl_page']['catalogSettings_legend'] = 'Catalog Manager Sett
 
 $GLOBALS['TL_LANG']['tl_page']['catalogMasterTable'] = [ 'Table', 'Please select a Table.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogChangeLanguageTable'] = [ 'Table', 'Please select a Table.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogUseRouting'] = [ 'Use Routing', 'Here you can overwrite routing behavior.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogUseMaster'] = [ 'Use Master', 'Here you can select a master table for your taxonomies settings.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogRouting'] = [ 'Routing', 'Here you can define new routing structure. E.g {myFieldname}/{auto_item}.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogDetermineRoutingTable'] = [ 'Restrict clean urls', 'Here you can restrict clean url for only one table.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogUseChangeLanguage'] = [ 'Translate items', 'This allows you to change the entries on a detail page via changelanguage module.' ];

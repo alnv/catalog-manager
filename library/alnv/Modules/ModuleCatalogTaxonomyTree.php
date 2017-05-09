ddb37b5b338886080a48586e246e4fca2bdd0d95
Alexander Naumov <me@alexandernaumov.de>
2017-05-09 10:24:41 +0200

    add blueprint taxonomy fe module class

---
+++ library/alnv/Modules/ModuleCatalogTaxonomyTree.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CatalogManager;
+
+class ModuleCatalogTaxonomyTree extends \Module {
+
+
+    protected $strTemplate = 'mod_catalog_taxonomy';
+
+
+    public function generate() {
+
+        if ( TL_MODE == 'BE' ) {
+
+            $objTemplate = new \BackendTemplate('be_wildcard');
+
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->title = $this->headline;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+            $objTemplate->wildcard = '### ' . utf8_strtoupper( $GLOBALS['TL_LANG']['FMD']['catalogFilter'][0] ) . ' ###';
+
+            return $objTemplate->parse();
+        }
+
+        return parent::generate();
+    }
+
+
+    protected function compile() {
+
+        $this->Import('CatalogTaxonomy');
+
+        $this->CatalogTaxonomy->initialize();
+    }
+}

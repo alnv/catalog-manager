adac7f0e0d89ea9499dc2dc926cb3e943b683cd4
Alexander Naumov <me@alexandernaumov.de>
2017-05-09 12:12:33 +0200

    improve routing setter and add new fields

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -1,12 +1,14 @@
 <?php
 
+$GLOBALS['TL_DCA']['tl_page']['config']['onsubmit_callback'][] = [ 'CatalogManager\tl_page', 'setRoutingParameter' ];
+
 $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'catalogUseMaster';
 $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'catalogUseRouting';
 $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'catalogUseChangeLanguage';
 
 $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace( 'includeLayout;', 'includeLayout;{catalogSettings_legend},catalogUseMaster,catalogUseChangeLanguage,catalogUseRouting;', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] );
 
-$GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseRouting'] = 'catalogRouting';
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseRouting'] = 'catalogRoutingTable,catalogRoutingParameter,catalogSetAutoItem,catalogRouting';
 $GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseMaster'] = 'catalogMasterTable';
 $GLOBALS['TL_DCA']['tl_page']['subpalettes']['catalogUseChangeLanguage'] = 'catalogChangeLanguageTable';
 
@@ -48,6 +50,44 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['catalogUseRouting'] = [
     'eval' => [
 
         'tl_class' => 'clr',
+        'doNotCopy' => true,
+        'submitOnChange' => true
+    ],
+
+    'exclude' => true,
+    'sql' => "char(1) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['catalogRoutingParameter'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_page']['catalogRoutingParameter'],
+    'inputType' => 'checkboxWizard',
+
+    'eval' => [
+
+        'multiple' => true,
+        'tl_class' => 'clr',
+        'mandatory' => true,
+        'doNotCopy' => true,
+        'submitOnChange' => true,
+
+    ],
+
+    'options_callback' => [ 'CatalogManager\tl_page', 'getRoutingFields' ],
+
+    'exclude' => true,
+    'sql' => "varchar(256) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['catalogSetAutoItem'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_page']['catalogSetAutoItem'],
+    'inputType' => 'checkbox',
+
+    'eval' => [
+
+        'tl_class' => 'clr',
+        'doNotCopy' => true,
         'submitOnChange' => true
     ],
 
@@ -62,10 +102,9 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['catalogRouting'] = [
 
     'eval' => [
 
-        'maxlength' => 128,
-        'tl_class' => 'long',
-        'mandatory' => true,
+        'readonly' => true,
         'doNotCopy' => true,
+        'tl_class' => 'long'
     ],
 
     'exclude' => true,
@@ -110,6 +149,29 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['catalogMasterTable'] = [
         'includeBlankOption' => true
     ],
     
+    'options_callback' => [ 'CatalogManager\tl_page', 'getCatalogTables' ],
+
+    'exclude' => true,
+    'sql' => "varchar(128) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['catalogRoutingTable'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_page']['catalogRoutingTable'],
+    'inputType' => 'select',
+
+    'eval' => [
+
+        'chosen' => true,
+        'maxlength' => 128,
+        'tl_class' => 'clr',
+        'mandatory' => true,
+        'doNotCopy' => true,
+        'submitOnChange' => true,
+        'blankOptionLabel' => '-',
+        'includeBlankOption' => true
+    ],
+
     'options_callback' => [ 'CatalogManager\tl_page', 'getCatalogTables' ],
 
     'exclude' => true,

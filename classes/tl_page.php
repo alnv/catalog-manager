ddfd1f7708969c1aade9a51692937a2ff061ec74
Alexander Naumov <me@alexandernaumov.de>
2017-05-09 12:12:10 +0200

    implement routing setter

--- classes/tl_page.php
+++ classes/tl_page.php
@@ -24,4 +24,59 @@ class tl_page extends \Backend {
 
         return $this->arrCatalogCache;
     }
+
+
+    public function getRoutingFields( \DataContainer $dc ) {
+
+        if ( !$dc->activeRecord->catalogRoutingTable ) return [];
+
+        $arrReturn = [];
+        $objCatalogFields = $this->Database->prepare( 'SELECT * FROM tl_catalog_fields WHERE pid = ( SELECT id FROM tl_catalog WHERE tablename = ? )' )->execute( $dc->activeRecord->catalogRoutingTable );
+
+        while ( $objCatalogFields->next() ) {
+
+            if ( !$objCatalogFields->fieldname ) continue;
+
+            if ( !in_array( $objCatalogFields->type, [ 'select', 'radio', 'checkbox' ] ) ) {
+
+                continue;
+            }
+
+            $arrReturn[ $objCatalogFields->fieldname ] = $objCatalogFields->title ? $objCatalogFields->title . ' <span style="color:#333; font-size:12px; display:inline">[ ' . $objCatalogFields->fieldname . ' ]</span>': $objCatalogFields->fieldname;
+        }
+
+        return $arrReturn;
+    }
+
+
+    public function setRoutingParameter( \DataContainer $dc ) {
+
+        if ( $dc->id && $dc->activeRecord->catalogUseRouting ) {
+
+            $arrRoutingSchema = [];
+            $arrCatalogRoutingParameter = Toolkit::deserialize( $dc->activeRecord->catalogRoutingParameter );
+
+            if ( !empty( $arrCatalogRoutingParameter ) && is_array( $arrCatalogRoutingParameter ) ) {
+
+                foreach ( $arrCatalogRoutingParameter as $arrParameter ) {
+
+                    if ( $arrParameter ) {
+
+                        $arrRoutingSchema[] = '{' . $arrParameter . '}';
+                    }
+                }
+            }
+
+            if ( $dc->activeRecord->catalogSetAutoItem ) {
+
+                $arrRoutingSchema[] = '{auto_item}';
+            }
+
+            if ( !empty( $arrRoutingSchema ) && is_array( $arrRoutingSchema ) ) {
+
+                $strRoutingFragments = implode( '/', $arrRoutingSchema );
+                $this->Database->prepare( 'UPDATE tl_page SET catalogRouting = ? WHERE id = ?' )->execute( $strRoutingFragments, $dc->id );
+            }
+        }
+    }
 }

7404603395eaa1cc92df4fc16fa0463a760841f5
Alexander Naumov <me@alexandernaumov.de>
2017-05-09 10:24:54 +0200

    add empty taxonomy class

---
+++ library/alnv/CatalogTaxonomy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CatalogManager;
+
+class CatalogTaxonomy extends CatalogController {
+
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+
+    public function initialize() {
+
+        //
+    }
+}

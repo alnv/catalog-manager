1ac4656132dea831ce901783209a4e065a98e04e
Alexander Naumov <me@alexandernaumov.de>
2017-05-09 17:31:54 +0200

    implement parameter parser

--- library/alnv/CatalogTaxonomy.php
+++ library/alnv/CatalogTaxonomy.php
@@ -9,6 +9,8 @@ class CatalogTaxonomy extends CatalogController {
     public $strParameter = '';
     public $arrParameter = [];
 
+    protected $arrCatalog = [];
+    protected $arrCatalogFields = [];
 
     public function __construct() {
 
@@ -41,10 +43,86 @@ class CatalogTaxonomy extends CatalogController {
 
         if ( $this->strParameter ) {
 
-            $this->arrParameter = Toolkit::getRoutingParameter( $this->strParameter );
+            $this->arrParameter = Toolkit::getRoutingParameter( $this->strParameter, true );
         }
+
+        if ( !$this->catalogTablename ) return null;
+
+        $this->arrCatalog = $this->SQLQueryHelper->getCatalogByTablename( $this->catalogTablename );
+        $this->arrCatalogFields = $this->SQLQueryHelper->getCatalogFieldsByCatalogTablename( $this->catalogTablename );
     }
 
+
+    public function getTaxonomiesView() {
+
+        $strResults = '';
+        $arrParameters = array_keys( $this->arrParameter );
+        $objEntities = $this->SQLQueryHelper->SQLQueryBuilder->Database->prepare( sprintf( 'SELECT %s FROM %s', implode( ',', $arrParameters ), $this->catalogTablename ) )->execute();
+
+        if ( !$objEntities->numRows ) return '';
+
+        while ( $objEntities->next() ) {
+
+            $arrEntity =  $objEntities->row();
+
+            if ( !empty( $arrEntity ) && is_array( $arrEntity ) ) {
+
+                foreach ( $arrEntity as $strFieldname => $varValue ) {
+
+                    if ( !$varValue ) continue;
+
+                    if ( is_array( $this->arrParameter[ $strFieldname ] ) ) {
+
+                        $varOptions = $this->parseCatalogValues( $varValue, $strFieldname, $arrEntity );
+
+                        if ( is_array( $varOptions ) ) {
+
+                            $varOptions = implode( ',', $varOptions );
+                        }
+
+                        $this->arrParameter[ $strFieldname ][ $varValue ] = [
+
+                            'alias' => $varValue,
+                            'title' => $varOptions
+                        ];
+                    }
+                }
+            }
+        }
+
+        // @todo
+
+        return '';
+    }
+
+
+    public function parseCatalogValues( $varValue, $strFieldname, $arrCatalog ) {
+
+        $arrField = $this->arrCatalogFields[ $strFieldname ];
+
+        switch ( $arrField['type'] ) {
+
+            case 'select':
+
+                return Select::parseValue( $varValue, $arrField, $arrCatalog );
+
+                break;
+
+            case 'checkbox':
+
+                return Checkbox::parseValue( $varValue, $arrField, $arrCatalog );
+
+                break;
+
+            case 'radio':
+
+                return Radio::parseValue( $varValue, $arrField, $arrCatalog );
+
+                break;
+        }
+
+        return $varValue;
+    }
 
     protected function getParameterFromPage() {
 

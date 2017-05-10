d11e511de86524cad2ae852cf1bf015981f75b10
Alexander Naumov <me@alexandernaumov.de>
2017-05-10 16:51:06 +0200

    implement taxonomy tpl parser

--- library/alnv/CatalogTaxonomy.php
+++ library/alnv/CatalogTaxonomy.php
@@ -6,11 +6,15 @@ class CatalogTaxonomy extends CatalogController {
 
 
     public $arrOptions = [];
-    public $strParameter = '';
-    public $arrParameter = [];
 
+    protected $strName = '';
+    protected $arrActive = [];
     protected $arrCatalog = [];
+    protected $strParameter = '';
+    protected $arrParameter = [];
+    protected $arrTaxonomyTree = [];
     protected $arrCatalogFields = [];
+
 
     public function __construct() {
 
@@ -43,56 +47,135 @@ class CatalogTaxonomy extends CatalogController {
 
         if ( $this->strParameter ) {
 
-            $this->arrParameter = Toolkit::getRoutingParameter( $this->strParameter, true );
+            $this->arrParameter = array_keys( Toolkit::getRoutingParameter( $this->strParameter ) );
         }
 
         if ( !$this->catalogTablename ) return null;
 
         $this->arrCatalog = $this->SQLQueryHelper->getCatalogByTablename( $this->catalogTablename );
         $this->arrCatalogFields = $this->SQLQueryHelper->getCatalogFieldsByCatalogTablename( $this->catalogTablename );
+
+        $this->setTaxonomyTree();
     }
 
 
-    public function getTaxonomiesView() {
+    public function getTaxonomyTree( $strTree = 'arrTaxonomyTree' ) {
 
         $strResults = '';
-        $arrParameters = array_keys( $this->arrParameter );
-        $objEntities = $this->SQLQueryHelper->SQLQueryBuilder->Database->prepare( sprintf( 'SELECT %s FROM %s', implode( ',', $arrParameters ), $this->catalogTablename ) )->execute();
+        $arrTaxonomies = [];
 
-        if ( !$objEntities->numRows ) return '';
+        if ( !isset( $this->{$strTree} ) || empty( $this->{$strTree} ) || !is_array( $this->{$strTree} ) ) {
 
-        while ( $objEntities->next() ) {
-
-            $arrEntity =  $objEntities->row();
-
-            if ( !empty( $arrEntity ) && is_array( $arrEntity ) ) {
-
-                foreach ( $arrEntity as $strFieldname => $varValue ) {
-
-                    if ( !$varValue ) continue;
-
-                    if ( is_array( $this->arrParameter[ $strFieldname ] ) ) {
-
-                        $varOptions = $this->parseCatalogValues( $varValue, $strFieldname, $arrEntity );
-
-                        if ( is_array( $varOptions ) ) {
-
-                            $varOptions = implode( ',', $varOptions );
-                        }
-
-                        $this->arrParameter[ $strFieldname ][ $varValue ] = [
-
-                            'alias' => $varValue,
-                            'title' => $varOptions
-                        ];
-                    }
-                }
-            }
+            return $strResults;
         }
 
-        // @todo
+        foreach ( $this->{$strTree} as $arrTree ) {
 
-        return '';
+            $arrTaxonomies[] = $this->setSubTaxonomies( $arrTree );
+        }
+
+        $objTemplate = new \FrontendTemplate( 'ctlg_taxonomy_nav' );
+        $objTemplate->setData(['arrItems' => $arrTaxonomies ]);
+        $strResults .= $objTemplate->parse();
+
+        return $strResults;
+    }
+
+
+    protected function setSubTaxonomies( $arrTree ) {
+
+        if ( ( $arrTree['active'] && $arrTree['next'] ) && ( isset( $this->{$arrTree['next']} ) && is_array( $this->{$arrTree['next']} ) ) ) {
+
+           $arrTree['subItems'] = $this->getTaxonomyTree( $arrTree['next'] );
+        }
+
+        return $arrTree;
+    }
+
+
+    protected function setTaxonomyTree() {
+
+        global $objPage;
+
+        $strTaxonomyUrl = '';
+        $strTempParameter = '';
+
+        foreach ( $this->arrParameter as $intIndex => $strParameter ) {
+
+            $strNextParameter = $this->arrParameter[ $intIndex + 1 ] ? $this->arrParameter[ $intIndex + 1 ] : '';
+
+            if ( !$intIndex ) {
+
+                $objEntities = $this->SQLQueryHelper->SQLQueryBuilder->Database->prepare( sprintf( 'SELECT DISTINCT %s FROM %s', $strParameter, $this->catalogTablename ) )->execute();
+
+                if ( !$objEntities->numRows ) continue;
+
+                while ( $objEntities->next() ) {
+
+                    if ( !$objEntities->{$strParameter} ) continue;
+
+                    $varOptions = $this->parseCatalogValues( $objEntities->{$strParameter}, $strParameter, [] );
+
+                    if ( is_array( $varOptions ) ) $varOptions = implode( ',', $varOptions );
+
+                    $this->arrTaxonomyTree[ $objEntities->{$strParameter} ] = [
+
+                        'title' => $varOptions,
+                        'next' => $strNextParameter,
+                        'alias' => $objEntities->{$strParameter},
+                        'href' => $this->generateUrl( $objPage, '/' . $objEntities->{$strParameter} ),
+                        'class' => \Input::get( $strParameter ) == $objEntities->{$strParameter} ? 'active' : '',
+                        'active' => \Input::get( $strParameter ) == $objEntities->{$strParameter} ? true : false,
+                    ];
+                }
+
+                $this->strName = $strParameter;
+            }
+
+            if ( $intIndex || ( \Input::get( $strTempParameter ) && $strTempParameter ) ) {
+
+                $objEntities = $this->SQLQueryHelper->SQLQueryBuilder->Database
+                    ->prepare( sprintf( 'SELECT DISTINCT %s FROM %s WHERE FIND_IN_SET( ?, LOWER( CAST( %s AS CHAR ) ) ) AND FIND_IN_SET( ?, LOWER( CAST( %s AS CHAR ) ) ) ', $strParameter, $this->catalogTablename, $strTempParameter, $this->strName ) )
+                    ->execute( \Input::get( $strTempParameter ), \Input::get( $this->strName ) );
+
+                if ( !$objEntities->numRows ) continue;
+
+                if ( !isset( $this->{$strParameter} ) ) $this->{$strParameter} = [];
+
+                $strTaxonomyUrl .= $strTaxonomyUrl ? '/' . \Input::get( $strTempParameter ) : '/' . \Input::get( $strTempParameter );
+
+                while ( $objEntities->next() ) {
+
+                    if ( !$objEntities->{$strParameter} ) continue;
+
+                    $varOptions = $this->parseCatalogValues( $objEntities->{$strParameter}, $strParameter, [] );
+
+                    if ( is_array( $varOptions ) ) $varOptions = implode( ',', $varOptions );
+
+                    $this->{$strParameter}[] = [
+
+                        'title' => $varOptions,
+                        'next' => $strNextParameter,
+                        'alias' => $objEntities->{$strParameter},
+                        'class' => \Input::get( $strParameter ) == $objEntities->{$strParameter} ? 'active' : '',
+                        'active' => \Input::get( $strParameter ) == $objEntities->{$strParameter} ? true : false,
+                        'href' =>  $this->generateUrl( $objPage, $strTaxonomyUrl . '/' . $objEntities->{$strParameter} ),
+                    ];
+                }
+            }
+
+            $strTempParameter = $strParameter;
+
+            if ( \Input::get( $strParameter ) ) $this->arrActive[] = $strParameter;
+        }
+    }
+
+
+    protected function generateUrl( $objPage, $strAlias ) {
+
+        if ( $objPage == null ) return '';
+
+        return $this->generateFrontendUrl( $objPage->row(), $strAlias );
     }
 
 
@@ -124,6 +207,7 @@ class CatalogTaxonomy extends CatalogController {
         return $varValue;
     }
 
+
     protected function getParameterFromPage() {
 
         if ( !$this->catalogPageRouting ) return null;
@@ -132,7 +216,11 @@ class CatalogTaxonomy extends CatalogController {
 
         if ( !$objPage->numRows ) return null;
 
-        $this->strParameter = str_replace( '{auto_item}', '', $objPage->catalogRouting );
+        if ( $objPage->catalogUseRouting ) {
+
+            $this->strParameter = str_replace( '{auto_item}', '', $objPage->catalogRouting );
+        }
+
         $this->catalogTablename = $objPage->catalogRoutingTable;
     }
 

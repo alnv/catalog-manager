6a7f5bb50117b219e7037b73191001f41fb3a108
Alexander Naumov <me@alexandernaumov.de>
2017-05-31 13:39:02 +0200

    add labels for form filter module

---
+++ languages/de/tl_catalog_form.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_form']['new'] = [ 'Filter Formular erstellen', 'Hier können Sie ein Filter Formular erstellen.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_form']['delete'] = [ 'Filter Formular löschen', 'Filter Formular mit der ID %s löschen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['edit'] = [ 'Filter Formular bearbeiten', 'Filter Formular mit der ID %s bearbeiten.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['show'] = [ 'Details des Filter Formulars anzeigen', 'Die Details des Filter Formulars mit der ID %s anzeigen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['editFields'] = [ 'Filter Formular Felder bearbeiten', 'Die Felder des Filter Formulars mit der ID %s bearbeiten.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_form']['catalog'] = [ 'Backend Modul', 'Bitte wählen Sie ein Backend Modul aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['title'] = [ 'Bezeichnung', 'Bitte geben Sie eine Bezeichnung für diesen Filter Formular ein.' ];
---
+++ languages/en/tl_catalog_form.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_form']['new'] = [ 'Create filter form', 'Here you can create new filter form.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_form']['delete'] = [ 'Delete filter form', 'Delete filter form ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['edit'] = [ 'Edit filter form', 'Edit filter form ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['show'] = [ 'Show details', 'Show the details of filter form ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['editFields'] = [ 'Edit filter form fields', 'Edit the fields of filter form ID "%s".' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_form']['title'] = [ 'Title', 'Please enter a filter form title.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['catalog'] = [ 'Backend Module', 'Please select a backend module.' ];

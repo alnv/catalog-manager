021718b5f92e32fc197b0d7dbc7a65b873c61b9e
Alexander Naumov <me@alexandernaumov.de>
2017-05-31 16:12:56 +0200

    add label

---
+++ languages/de/tl_catalog_form_fields.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_form_fields']['type'] = [ 'Feldtyp', 'Bitte wählen Sie ein Feldtyp aus.' ];
---
+++ languages/en/tl_catalog_form_fields.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_form_fields']['type'] = [ 'Field type', 'Please select a field type.' ];

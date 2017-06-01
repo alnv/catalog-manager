1fa6eb434220694f5ed6d238ad3f0273db362dfe
Alexander Naumov <me@alexandernaumov.de>
2017-06-01 15:39:11 +0200

    add ce class and empty methods

---
+++ classes/tl_content.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CatalogManager;
+
+class tl_content extends \Backend {
+
+
+    public function getCatalogForms() {
+
+        return [];
+    }
+
+
+    public function editCatalogForm() {
+
+        return '';
+    }
+}

d666ef5784e368f1fc7819a106008470971f2793
Alexander Naumov <me@alexandernaumov.de>
2017-06-01 15:39:23 +0200

    add form filter palette

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -1,5 +1,28 @@
 <?php
 
+$GLOBALS['TL_DCA']['tl_content']['palettes']['catalogFilterForm'] = '{type_legend},type,headline;{include_legend},catalogForm;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['catalogForm'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['catalogForm'],
+    'inputType' => 'select',
+
+    'eval' => [
+
+        'chosen' => true,
+        'mandatory' => true,
+        'tl_class' => 'w50',
+        'blankOptionLabel' => '-',
+        'includeBlankOption' => true
+    ],
+
+    'wizard' => [ 'CatalogManager\tl_content', 'getCatalogForms' ],
+    'options_callback' => [ 'CatalogManager\tl_content', 'editCatalogForm' ],
+
+    'exclude' => true,
+    'sql' => "int(10) unsigned NOT NULL default '0'"
+];
+
 if ( \Input::get('do') ) {
     
     $arrCatalogs = $GLOBALS['TL_CATALOG_MANAGER']['CATALOG_EXTENSIONS'];

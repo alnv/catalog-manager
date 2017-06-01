0cf5294ee32d5a3d628e6c73a35be5f9708f4e18
Alexander Naumov <me@alexandernaumov.de>
2017-06-01 15:38:19 +0200

    add form getter label

---
+++ languages/de/tl_content.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['catalogForm'] = [ 'Formular', 'Bitte wählen Sie ein Formular.' ];
---
+++ languages/en/tl_content.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['catalogForm'] = [ 'Form', 'Please select a form.' ];

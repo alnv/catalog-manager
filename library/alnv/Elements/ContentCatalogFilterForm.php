b7bbeedcbf8fc1695cb60c2eb3c5f388fa4bf0ed
Alexander Naumov <me@alexandernaumov.de>
2017-06-01 15:38:36 +0200

    add empty filter form ce class

---
+++ library/alnv/Elements/ContentCatalogFilterForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CatalogManager;
+
+class ContentCatalogFilterForm extends \ContentElement {
+
+
+    protected $strTemplate = 'ce_catalog_filterform';
+
+
+    protected function compile() {
+
+        
+    }
+}

b7fbcae30f4d489f254d463f13bbed7501c3758c
Alexander Naumov <me@alexandernaumov.de>
2017-06-02 10:47:19 +0200

    implement widget and form getter

--- classes/tl_content.php
+++ classes/tl_content.php
@@ -5,14 +5,39 @@ namespace CatalogManager;
 class tl_content extends \Backend {
 
 
-    public function getCatalogForms() {
+    public function __construct() {
 
-        return [];
+        parent::__construct();
+
+        $this->import( 'BackendUser', 'User' );
     }
 
 
-    public function editCatalogForm() {
+    public function getCatalogForms() {
 
-        return '';
+        $arrForms = [];
+
+        if ( !$this->User->isAdmin && !is_array( $this->User->filterform ) ) {
+
+            return $arrForms;
+        }
+
+        $objForms = $this->Database->execute("SELECT id, title FROM tl_catalog_form ORDER BY title");
+
+        while ( $objForms->next() ) {
+
+            if ( $this->User->hasAccess( $objForms->id, 'filterform' ) ) {
+
+                $arrForms[ $objForms->id ] = $objForms->title . ' (ID ' . $objForms->id . ')';
+            }
+        }
+
+        return $arrForms;
+    }
+
+
+    public function editCatalogForm( \DataContainer $dc ) {
+
+        return ( $dc->value < 1 ) ? '' : ' <a href="contao/main.php?do=filterform&amp;table=tl_catalog_form_fields&amp;id=' . $dc->value . '&amp;popup=1&amp;nb=1&amp;rt=' . REQUEST_TOKEN . '" title="' . sprintf(specialchars($GLOBALS['TL_LANG']['tl_content']['editalias'][1]), $dc->value) . '" style="padding-left:3px" onclick="Backend.openModalIframe({\'width\':768,\'title\':\'' . specialchars(str_replace("'", "\\'", sprintf($GLOBALS['TL_LANG']['tl_content']['editalias'][1], $dc->value))) . '\',\'url\':this.href});return false">' . \Image::getHtml('alias.gif', $GLOBALS['TL_LANG']['tl_content']['editalias'][0], 'style="vertical-align:top"') . '</a>';
     }
 }

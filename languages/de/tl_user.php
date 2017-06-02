c9c38938466ea74496d9e2c2e8cddf2dff4d3298
Alexander Naumov <me@alexandernaumov.de>
2017-06-02 10:45:19 +0200

    add permission field labels

--- languages/de/tl_user.php
+++ languages/de/tl_user.php
@@ -1,9 +1,13 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_user']['filterform_legend'] = 'Filtergenerator-Berechtigungen';
 $GLOBALS['TL_LANG']['tl_user']['catalog_manager_legend'] = 'Catalog Manager-Berechtigungen';
 
 $GLOBALS['TL_LANG']['tl_user']['catalog'] = [ 'Catalog Manager', 'Hier können Sie den Zugriff auf eine oder mehrere Kataloge erlauben.' ];
-$GLOBALS['TL_LANG']['tl_user']['catalogp'] = [ 'Catalog Manager permissions', 'Hier können Sie die Katalog Rechte festlegen.' ];
+$GLOBALS['TL_LANG']['tl_user']['catalogp'] = [ 'Catalog Manager Rechte', 'Hier können Sie die Katalog Rechte festlegen.' ];
+
+$GLOBALS['TL_LANG']['tl_user']['filterform'] = [ 'Filtergenerator', 'Hier können Sie den Zugriff auf eine oder mehrere Filter Formulare erlauben.' ];
+$GLOBALS['TL_LANG']['tl_user']['filterformp'] = [ 'Filtergenerator Rechte', 'Hier können Sie die Formular Rechte festlegen.' ];
 
 $GLOBALS['TL_LANG']['tl_user']['reference'] = [
 

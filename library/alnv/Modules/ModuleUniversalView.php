3158f09ee51562b96df8a3e57ac441417a51bc7d
Alexander Naumov <me@alexandernaumov.de>
2017-06-06 16:05:07 +0200

    add master id to addComment helper function

--- library/alnv/Modules/ModuleUniversalView.php
+++ library/alnv/Modules/ModuleUniversalView.php
@@ -195,7 +195,6 @@ class ModuleUniversalView extends \Module {
         $this->CatalogView->objMainTemplate = $this->Template;
         $this->CatalogView->strTemplate = $this->catalogMasterTemplate ? $this->catalogMasterTemplate : 'catalog_master';
         $this->CatalogView->initialize();
-        $this->CatalogView->getCommentForm();
 
         if ( !$this->CatalogView->checkPermission() ) {
 
@@ -206,7 +205,8 @@ class ModuleUniversalView extends \Module {
         }
 
         $strOutput = $this->CatalogView->getCatalogView( $arrQuery );
-
+        $this->CatalogView->getCommentForm( $this->CatalogView->strMasterID );
+        
         if ( empty( $strOutput ) ) {
 
             $objHandler = new $GLOBALS['TL_PTY']['error_404']();

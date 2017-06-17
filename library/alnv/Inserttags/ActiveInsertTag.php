ed6d176b93bc420419c6e4481ea78281c617aaae
Alexander Naumov <me@alexandernaumov.de>
2017-06-17 13:19:10 +0200

    implement post support

--- library/alnv/Inserttags/ActiveInsertTag.php
+++ library/alnv/Inserttags/ActiveInsertTag.php
@@ -14,7 +14,7 @@ class ActiveInsertTag extends \Frontend {
             $varValue =  '';
 
             if ( \Input::get( $arrTags[1] ) ) $varValue = \Input::get( $arrTags[1] );
-            if ( \Input::post( $arrTags[1] ) ) $varValue = \Input::post( $arrTags[1] );
+            if ( \Input::post( $arrTags[1] ) ) $varValue = \Input::post( $arrTags[1] ); # see #13
             if ( !$varValue ) $varValue = $arrTags[2] ? $arrTags[2] : '';
             if ( is_array( $varValue ) ) $varValue = implode( ',', $varValue );
 

afc2623c8990587f10865cf21331bc56bf3d7842
Alexander Naumov <me@alexandernaumov.de>
2017-06-17 12:22:16 +0200

    get post values too

--- library/alnv/Inserttags/ActiveInsertTag.php
+++ library/alnv/Inserttags/ActiveInsertTag.php
@@ -11,13 +11,12 @@ class ActiveInsertTag extends \Frontend {
 
         if ( is_array( $arrTags ) && $arrTags[0] == 'CTLG_ACTIVE' && isset( $arrTags[1] ) ) {
 
-            $varValue = $arrTags[2] ? $arrTags[2] : '';
-            $varValue =  \Input::get( $arrTags[1] ) ? \Input::get( $arrTags[1] ) : $varValue;
+            $varValue =  '';
 
-            if ( is_array( $varValue ) ) {
-
-                $varValue = implode( ',', $varValue );
-            }
+            if ( \Input::get( $arrTags[1] ) ) $varValue = \Input::get( $arrTags[1] );
+            if ( \Input::post( $arrTags[1] ) ) $varValue = \Input::post( $arrTags[1] );
+            if ( !$varValue ) $varValue = $arrTags[2] ? $arrTags[2] : '';
+            if ( is_array( $varValue ) ) $varValue = implode( ',', $varValue );
 
             return $varValue;
         }

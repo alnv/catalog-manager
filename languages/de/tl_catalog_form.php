b7988b8ae9331f9a9641b515bd51da13325ac3c9
Alexander Naumov <me@alexandernaumov.de>
2017-06-20 17:07:38 +0200

    add missing labels

--- languages/de/tl_catalog_form.php
+++ languages/de/tl_catalog_form.php
@@ -1,5 +1,8 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_catalog_form']['expert_legend'] = 'Experten-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog_form']['general_legend'] = 'Allgemeine-Einstellungen';
+
 $GLOBALS['TL_LANG']['tl_catalog_form']['new'] = [ 'Filter Formular erstellen', 'Hier können Sie ein Filter Formular erstellen.' ];
 
 $GLOBALS['TL_LANG']['tl_catalog_form']['delete'] = [ 'Filter Formular löschen', 'Filter Formular mit der ID %s löschen.' ];
@@ -7,5 +10,9 @@ $GLOBALS['TL_LANG']['tl_catalog_form']['edit'] = [ 'Filter Formular bearbeiten',
 $GLOBALS['TL_LANG']['tl_catalog_form']['show'] = [ 'Details des Filter Formulars anzeigen', 'Die Details des Filter Formulars mit der ID %s anzeigen.' ];
 $GLOBALS['TL_LANG']['tl_catalog_form']['editFields'] = [ 'Filter Formular Felder bearbeiten', 'Die Felder des Filter Formulars mit der ID %s bearbeiten.' ];
 
-$GLOBALS['TL_LANG']['tl_catalog_form']['catalog'] = [ 'Backend Modul', 'Bitte wählen Sie ein Backend Modul aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['method'] = [ 'Übertragungsmethode', 'Die Standard-Übertragungsmethode ist GET.' ];
 $GLOBALS['TL_LANG']['tl_catalog_form']['title'] = [ 'Bezeichnung', 'Bitte geben Sie eine Bezeichnung für diesen Filter Formular ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['formID'] = [ 'Formular-ID', 'Die Formular-ID wird zur Ansteuerung eines Contao-Moduls benötigt.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['attributes'] = [ 'CSS-ID/Klasse', 'Hier können Sie eine ID und beliebig viele Klassen eingeben.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['resetForm'] = [ 'Filtereinstellungen zurücksetzen erlauben', 'Die Besucher dürfen die Filtereinstellungen zurücksetzen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['jumpTo'] = [ 'Weiterleitungsseite', 'Bitte wählen Sie die Seite aus, zu der Besucher nach dem Abschicken des Formulars weitergeleitet werden.' ];
--- languages/en/tl_catalog_form.php
+++ languages/en/tl_catalog_form.php
@@ -1,5 +1,8 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_catalog_form']['expert_legend'] = 'Expert settings';
+$GLOBALS['TL_LANG']['tl_catalog_form']['general_legend'] = 'General settings';
+
 $GLOBALS['TL_LANG']['tl_catalog_form']['new'] = [ 'Create filter form', 'Here you can create new filter form.' ];
 
 $GLOBALS['TL_LANG']['tl_catalog_form']['delete'] = [ 'Delete filter form', 'Delete filter form ID "%s".' ];
@@ -8,4 +11,8 @@ $GLOBALS['TL_LANG']['tl_catalog_form']['show'] = [ 'Show details', 'Show the det
 $GLOBALS['TL_LANG']['tl_catalog_form']['editFields'] = [ 'Edit filter form fields', 'Edit the fields of filter form ID "%s".' ];
 
 $GLOBALS['TL_LANG']['tl_catalog_form']['title'] = [ 'Title', 'Please enter a filter form title.' ];
-$GLOBALS['TL_LANG']['tl_catalog_form']['catalog'] = [ 'Backend Module', 'Please select a backend module.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['formID'] = [ 'Form ID', 'The form ID is required to trigger a Contao module.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['method'] = [ 'Submission method', 'The default form submission method is GET.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['attributes'] = [ 'CSS ID/class', 'Here you can set an ID and one or more classes.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['resetForm'] = [ 'Add reset button to form', 'Allow to reset form filter in Frontend.' ];
+$GLOBALS['TL_LANG']['tl_catalog_form']['jumpTo'] = [ 'Redirect page', 'Please choose the page to which visitors will be redirected after submitting the form.' ];

30c239825f00eb1cfd8cf7757104a431cf28ffef
Alexander Naumov <me@alexandernaumov.de>
2017-06-27 16:27:23 +0200

    improve message getter

--- library/alnv/CatalogMessage.php
+++ library/alnv/CatalogMessage.php
@@ -15,7 +15,7 @@ class CatalogMessage extends CatalogController {
         $this->import( 'Input' );
     }
 
-    public function set( $strType, $arrData = [] ) {
+    public function set( $strType, $arrData = [], $strID ) {
 
         $strMessage = $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER'][ $strType ] ? $GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER'][ $strType ] : $strType;
         $objTemplate = new \FrontendTemplate( $this->strTemplate );
@@ -28,15 +28,16 @@ class CatalogMessage extends CatalogController {
 
         $objTemplate->setData( $arrTemplate );
         $strMessageTemplate = $objTemplate->parse();
-        \System::setCookie( 'ctlg_FEE_Message', $strMessageTemplate, time() + 1000  );
+
+        \System::setCookie( 'ctlg_FEE_Message' . ( $strID ? $strID : '' ) , $strMessageTemplate, time() + 1000  );
     }
 
 
-    public function get() {
+    public function get( $strID = '' ) {
 
-        $strMessage = $this->Input->cookie( 'ctlg_FEE_Message' );
+        $strMessage = $this->Input->cookie( 'ctlg_FEE_Message' . ( $strID ? $strID : '' ) );
         if ( !$strMessage ) $strMessage = '';
-        \System::setCookie( 'ctlg_FEE_Message', '', 0 );
+        \System::setCookie( 'ctlg_FEE_Message' . ( $strID ? $strID : '' ), '', 0 );
         return \StringUtil::decodeEntities( $strMessage );
     }
 }

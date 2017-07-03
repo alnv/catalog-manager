ae3ae299702a9e801c1c3155f1cfcbcebc75b371
Alexander Naumov <me@alexandernaumov.de>
2017-07-03 15:08:10 +0200

    rename class name

--- library/alnv/Forms/CatalogFineUploaderForm.php
+++ library/alnv/Forms/CatalogFineUploaderForm.php
@@ -2,10 +2,12 @@
 
 namespace CatalogManager;
 
-class CatalogFineUploader extends \Widget {
+class CatalogFineUploaderForm extends \Widget {
 
 
+    protected $blnSubmitInput = true;
     protected $strTemplate = 'ctlg_form_fine_uploader';
+    protected $strPrefix = 'widget widget-fine-uploader';
 
 
     public function validate() {

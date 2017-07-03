1f0df67beda3bf89e705b7af251cd6a362d2b0cb
Alexander Naumov <me@alexandernaumov.de>
2017-07-03 11:18:59 +0200

    add fine upload form field

---
+++ library/alnv/Forms/CatalogFineUploader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CatalogManager;
+
+class CatalogFineUploader extends \Widget {
+
+
+    protected $strTemplate = 'ctlg_form_fine_uploader';
+
+
+    public function validate() {
+
+        return null;
+    }
+
+
+    public function generate() {
+
+        return '';
+    }
+}

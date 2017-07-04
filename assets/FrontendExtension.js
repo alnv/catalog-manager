de97cca9310e05a7b770039851811b32a8b95908
Alexander Naumov <me@alexandernaumov.de>
2017-07-04 10:00:05 +0200

    implement fine uploader handler

--- assets/FrontendExtension.js
+++ assets/FrontendExtension.js
@@ -1,15 +1,96 @@
+
+var CatalogManager = CatalogManager || {};
+
 (function () {
 
     'use strict';
 
-    if ( typeof window.addEventListener != "undefined" ) {
+    CatalogManager.fineUploader =  function ( objElement, objOptions ) {
 
-        window.addEventListener( 'DOMContentLoaded', initialize, false );
+        objOptions = getOptions( objOptions );
+
+        var objParams = {
+
+            validation: {},
+            element: objElement,
+            debug: objOptions.debug,
+            multiple: objOptions.multiple
+        };
+
+        if ( !isEmpty( objOptions.sizeLimit ) && objOptions.sizeLimit != '0' ) {
+
+            objParams.validation.sizeLimit = objOptions.sizeLimit;
+        }
+
+        if ( !isEmpty( objOptions.allowedExtensions ) && objOptions.allowedExtensions.length ) {
+
+            var arrAllowedExtensions = [];
+
+            for ( var i = 0; i < objOptions.allowedExtensions.length; i++ ) {
+
+                if ( !isEmpty( objOptions.allowedExtensions[i] ) ) {
+
+                    arrAllowedExtensions.push( objOptions.allowedExtensions[i] );
+                }
+            }
+
+            if ( arrAllowedExtensions.length ) {
+
+                objParams.validation.allowedExtensions = arrAllowedExtensions;
+            }
+        }
+
+        return new qq.FineUploader( objParams );
+    };
+
+
+    function getOptions( objOptions ) {
+
+        var objDefault = {
+
+            debug: false,
+            sizeLimit: '0',
+            multiple: false
+        };
+
+        for ( var strOption in objDefault ) {
+
+            if ( objDefault.hasOwnProperty( strOption ) ) {
+
+                if ( isEmpty( objOptions[ strOption ] ) ) {
+
+                    objOptions[ strOption ] = objDefault[ strOption ];
+                }
+            }
+        }
+
+        return objOptions;
     }
 
-    function initialize() {
 
-        // @todo
+    function isEmpty( varValue ) {
+
+        if ( typeof varValue === 'undefined' ) {
+
+            return true;
+        }
+
+        if ( varValue === null && typeof varValue === 'object' ) {
+
+            return true;
+        }
+
+        if ( typeof varValue === 'string' && !varValue.length ) {
+
+            return true;
+        }
+
+        if ( typeof varValue === 'object' && typeof varValue.length != 'undefined' ) {
+
+            if ( !varValue.length ) return true;
+        }
+
+        return false;
     }
 
 })();

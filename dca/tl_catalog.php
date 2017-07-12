0fec4b431fb64a40429b11efa5477d685fd8409f
Alexander Naumov <me@alexandernaumov.de>
2017-07-12 15:04:35 +0200

    add multiple edit operator

--- dca/tl_catalog.php
+++ dca/tl_catalog.php
@@ -84,6 +84,17 @@ $GLOBALS['TL_DCA']['tl_catalog'] = [
                 'href' => 'act=show',
                 'icon' => 'show.gif'
             ]
+        ],
+
+        'global_operations' => [
+
+            'all' => [
+
+                'label' => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href' => 'act=select',
+                'class' => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            ]
         ]
     ],
 

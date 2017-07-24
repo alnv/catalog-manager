f05e35f8778867c53729e5b858885039c9c75a11
Alexander Naumov <me@alexandernaumov.de>
2017-07-24 16:42:48 +0200

    implement default value

--- library/alnv/Inserttags/MasterInsertTag.php
+++ library/alnv/Inserttags/MasterInsertTag.php
@@ -14,25 +14,34 @@ class MasterInsertTag extends \Frontend {
 
         $arrTags = explode( '::', $strTag );
 
-        if ( is_array( $arrTags ) && $arrTags[0] == 'CTLG_MASTER' && isset( $arrTags[1] ) ) {
+        if ( is_array( $arrTags ) && $arrTags[0] == 'CTLG_MASTER' && isset( $arrTags[1] ) && $objPage->catalogUseMaster ) {
 
             if ( $this->arrItemCache[ $objPage->catalogMasterTable ] ) {
 
-                return $this->arrItemCache[ $objPage->catalogMasterTable ][ $arrTags[1] ];
-            }
-            
-            if ( $objPage->catalogUseMaster && \Input::get('auto_item') ) {
+                $strValue = $this->arrItemCache[ $objPage->catalogMasterTable ][ $arrTags[1] ];
 
-                $arrMaster = $this->Database->prepare( sprintf( 'SELECT * FROM %s WHERE `alias`=? OR `id`=?', $objPage->catalogMasterTable ) )->limit(1)->execute( \Input::get('auto_item'), (int)\Input::get('auto_item') )->row();
-                $this->arrItemCache[ $objPage->catalogMasterTable ] = $arrMaster;
+                if ( Toolkit::isEmpty( $strValue ) && isset( $arrTags[2] ) ) {
 
-                if ( isset( $arrMaster[ $arrTags[1] ] ) ) {
-
-                    return $arrMaster[ $arrTags[1] ];
+                    $strValue = $arrTags[2];
                 }
 
-                return false;
+                return $strValue;
             }
+
+            $arrMaster = $this->Database->prepare( sprintf( 'SELECT * FROM %s WHERE `alias`=? OR `id`=?', $objPage->catalogMasterTable ) )->limit(1)->execute( \Input::get('auto_item'), (int)\Input::get('auto_item') )->row();
+            $this->arrItemCache[ $objPage->catalogMasterTable ] = $arrMaster;
+
+            if ( !Toolkit::isEmpty( $arrMaster[ $arrTags[1] ] ) ) {
+
+                return $arrMaster[ $arrTags[1] ];
+            }
+
+            elseif ( isset( $arrTags[2] ) ) {
+
+                return $arrTags[2];
+            }
+
+            return false;
         }
 
         return false;

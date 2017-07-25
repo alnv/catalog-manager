59de0a6c066f6fec722c3990afdf868c0a7b4132
Alexander Naumov <me@alexandernaumov.de>
2017-07-25 09:14:55 +0200

    improve master inserttag

--- library/alnv/Inserttags/MasterInsertTag.php
+++ library/alnv/Inserttags/MasterInsertTag.php
@@ -5,8 +5,8 @@ namespace CatalogManager;
 class MasterInsertTag extends \Frontend {
 
 
-    private $arrItemCache = [];
-
+    protected $arrMaster = [];
+    
 
     public function getInsertTagValue( $strTag ) {
 
@@ -14,36 +14,47 @@ class MasterInsertTag extends \Frontend {
 
         $arrTags = explode( '::', $strTag );
 
-        if ( is_array( $arrTags ) && $arrTags[0] == 'CTLG_MASTER' && isset( $arrTags[1] ) && $objPage->catalogUseMaster ) {
-
-            if ( $this->arrItemCache[ $objPage->catalogMasterTable ] ) {
-
-                $strValue = $this->arrItemCache[ $objPage->catalogMasterTable ][ $arrTags[1] ];
-
-                if ( Toolkit::isEmpty( $strValue ) && isset( $arrTags[2] ) ) {
-
-                    $strValue = $arrTags[2];
-                }
-
-                return $strValue;
-            }
-
-            $arrMaster = $this->Database->prepare( sprintf( 'SELECT * FROM %s WHERE `alias`=? OR `id`=?', $objPage->catalogMasterTable ) )->limit(1)->execute( \Input::get('auto_item'), (int)\Input::get('auto_item') )->row();
-            $this->arrItemCache[ $objPage->catalogMasterTable ] = $arrMaster;
-
-            if ( !Toolkit::isEmpty( $arrMaster[ $arrTags[1] ] ) ) {
-
-                return $arrMaster[ $arrTags[1] ];
-            }
-
-            elseif ( isset( $arrTags[2] ) ) {
-
-                return $arrTags[2];
-            }
+        if ( empty( $arrTags ) || !is_array( $arrTags ) ) {
 
             return false;
         }
 
+        if ( isset( $arrTags[0] ) && $arrTags[0] == 'CTLG_MASTER' && $objPage->catalogUseMaster ) {
+
+            $strFieldname = $arrTags[1];
+            $strDefaultValue = $arrTags[2] ? $arrTags[2] : '';
+
+            if ( !$strFieldname || !$objPage->catalogMasterTable ) return false;
+
+            $this->getMasterEntity( $objPage->catalogMasterTable );
+
+            if ( Toolkit::isEmpty( $this->arrMaster[ $objPage->catalogMasterTable ][ $strFieldname ] ) && !Toolkit::isEmpty( $strDefaultValue ) ) {
+
+                return $strDefaultValue;
+            }
+
+            return $this->arrMaster[ $objPage->catalogMasterTable ][ $strFieldname ] ? $this->arrMaster[ $objPage->catalogMasterTable ][ $strFieldname ] : '';
+        }
+
         return false;
+    }
+
+
+    protected function getMasterEntity( $strTable ) {
+
+        $strAlias = \Input::get('auto_item');
+
+        if ( !empty( $this->arrMaster[ $strTable ] ) && is_array( $this->arrMaster[ $strTable ] ) ) {
+
+            return null;
+        }
+
+        if ( Toolkit::isEmpty( $strAlias ) ) $this->arrMaster[ $strTable ] = [];
+
+        $objMaster = $this->Database->prepare( sprintf( 'SELECT * FROM %s WHERE `alias`=? OR `id`=?', $strTable ) )->limit(1)->execute( $strAlias, (int)$strAlias );
+
+        if ( $objMaster->numRows ) $this->arrMaster[ $strTable ] = $objMaster->row();
+
+        if ( !isset( $this->arrMaster[ $strTable ] ) ) $this->arrMaster[ $strTable ] = [];
     }
 }

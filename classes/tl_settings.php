39e427de86d50c068f6b62b72417dd22001c3847
Alexander Naumov <me@alexandernaumov.de>
2017-08-21 17:06:28 +0200

    implement nav areas getter

--- classes/tl_settings.php
+++ classes/tl_settings.php
@@ -5,6 +5,16 @@ namespace CatalogManager;
 class tl_settings extends \Backend {
 
 
+    public function changeGlobals() {
+
+        if ( \Input::get( 'do' ) && \Input::get( 'do' ) == 'settings' ) {
+
+            $GLOBALS['TL_LANG']['MSC']['ow_key'] = &$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['ow_key'];
+            $GLOBALS['TL_LANG']['MSC']['ow_value'] = &$GLOBALS['TL_LANG']['MSC']['CATALOG_MANAGER']['ow_value'];
+        }
+    }
+
+
     public function verifyLicence( $varValue ) {
 
         $objCatalogManagerVerification = new CatalogManagerVerification();
@@ -18,5 +28,28 @@ class tl_settings extends \Backend {
         }
 
         return $varValue;
+    }
+
+
+    public function parseNavigationName( $varValue ) {
+
+        $arrReturn = [];
+        $arrValues = deserialize( $varValue );
+
+        if ( !empty( $arrValues ) && is_array( $arrValues ) ) {
+
+            foreach ( $arrValues as $arrValue ) {
+
+                $arrReturn[] = [
+
+                    'key' => \StringUtil::generateAlias( $arrValue['key'] ),
+                    'value' => $arrValue['value']
+                ];
+            }
+        }
+
+        $arrReturn = serialize( $arrReturn );
+
+        return $arrReturn;
     }
 }

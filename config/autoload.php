11395311b610dfeb3e816c37b0f523f50b5b2b50
Alexander Naumov <me@alexandernaumov.de>
2017-08-21 17:04:43 +0200

    add hook class for widget parsing

--- config/autoload.php
+++ config/autoload.php
@@ -75,10 +75,11 @@ ClassLoader::addClasses([
     'CatalogManager\CatalogFineUploaderForm' => 'system/modules/catalog-manager/library/alnv/Forms/CatalogFineUploaderForm.php',
     'CatalogManager\CatalogManagerVerification' => 'system/modules/catalog-manager/library/alnv/CatalogManagerVerification.php',
     'CatalogManager\FilterValuesInsertTag' => 'system/modules/catalog-manager/library/alnv/Inserttags/FilterValuesInsertTag.php',
+    'CatalogManager\CatalogWidgetAttributeParser' => 'system/modules/catalog-manager/library/alnv/CatalogWidgetAttributeParser.php',
     'CatalogManager\ContentCatalogFilterForm' => 'system/modules/catalog-manager/library/alnv/Elements/ContentCatalogFilterForm.php',
     'CatalogManager\ModuleCatalogTaxonomyTree' => 'system/modules/catalog-manager/library/alnv/Modules/ModuleCatalogTaxonomyTree.php',
     'CatalogManager\CatalogRelationRedirectWizard' => 'system/modules/catalog-manager/library/alnv/Widgets/CatalogRelationRedirectWizard.php',
-    'CatalogManager\CatalogFilterFieldSelectWizard' => 'system/modules/catalog-manager/library/alnv/Widgets/CatalogFilterFieldSelectWizard.php',
+    'CatalogManager\CatalogFilterFieldSelectWizard' => 'system/modules/catalog-manager/library/alnv/Widgets/CatalogFilterFieldSelectWizard.php'
 ]);
 
 TemplateLoader::addFiles([

7ad386e0205bcfbbd4af1397c9e19a936a30cf3f
Alexander Naumov <me@alexandernaumov.de>
2017-08-21 17:04:02 +0200

    add label

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -3,5 +3,6 @@
 $GLOBALS['TL_LANG']['tl_settings']['catalog_manager_settings'] = 'Catalog Manager-Einstellungen';
 
 $GLOBALS['TL_LANG']['tl_settings']['catalogLicence'] = [ 'Catalog Manager Lizenz', 'Bitte geben Sie Ihre Lizenz ein.' ];
+$GLOBALS['TL_LANG']['tl_settings']['catalogNavigationAreas'] = [ 'Navigationsbereiche', 'Hier können Sie die Navigationsbereiche verwalten.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'] = [ 'Google-Maps Server Key', 'Bitte geben Sie den Google-Maps Server Key ein.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'] = [ 'Google-Maps Client Key', 'Bitte geben Sie den Google-Maps Client Key ein.' ];
--- languages/en/tl_settings.php
+++ languages/en/tl_settings.php
@@ -3,5 +3,6 @@
 $GLOBALS['TL_LANG']['tl_settings']['catalog_manager_settings'] = 'Catalog Manager Settings';
 
 $GLOBALS['TL_LANG']['tl_settings']['catalogLicence'] = [ 'Catalog Manager licence', 'Please enter your Catalog Manager licence.' ];
+$GLOBALS['TL_LANG']['tl_settings']['catalogNavigationAreas'] = [ 'Navigation areas', 'Here you can edit contao navigation areas.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsServerKey'] = [ 'Google-Maps Server Key', 'Please enter a google maps server key.' ];
 $GLOBALS['TL_LANG']['tl_settings']['catalogGoogleMapsClientKey'] = [ 'Google-Maps Client Key', 'Please enter a google maps client key.' ];

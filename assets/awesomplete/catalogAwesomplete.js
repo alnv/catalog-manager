ca09ba9aa4fd33b60867ffe26273d9dbdd95b7e2
Alexander Naumov <me@alexandernaumov.de>
2017-08-23 10:34:33 +0200

    implement awesomplete handler

---
+++ assets/awesomplete/catalogAwesomplete.js
@@ -0,0 +1,51 @@
+(function () {
+
+    'use strict';
+
+    if ( typeof window.addEventListener != 'undefined' ) {
+
+        window.addEventListener( 'DOMContentLoaded', initialize, false );
+    }
+
+    function initialize() {
+
+        var arrFields = document.querySelectorAll( '.ctlg_awesomplete' );
+
+        if ( typeof arrFields != 'undefined' && arrFields.length ) {
+
+            var objOptions = {
+
+                end: 3,
+                start: 0,
+                startswith: ''
+            };
+
+            for ( var i = 0; i < arrFields.length; i++ ) {
+
+                var objField = arrFields[i];
+
+                objOptions.startswith = objField.dataset.startswith;
+                objOptions.end = objField.dataset.end ? objField.dataset.end : 3;
+                objOptions.start = objField.dataset.start ? objField.dataset.start : 0;
+
+                var objAwesompleteConfig = {};
+
+                if ( objOptions.startswith.length ) {
+
+                    objAwesompleteConfig.data = function ( strText, strInput ) {
+
+                        if ( strInput.substr( objOptions.start, objOptions.end ) == objOptions.startswith ) {
+
+                            return strText;
+                        }
+                    };
+
+                    objAwesompleteConfig.filter = Awesomplete.FILTER_STARTSWITH;
+                }
+
+                new Awesomplete( objField, objAwesompleteConfig );
+            }
+        }
+    }
+
+})();

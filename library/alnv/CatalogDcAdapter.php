4a3f12275ed94b652e0fce6e66637a82941b3710
Alexander Naumov <me@alexandernaumov.de>
2017-08-28 15:44:56 +0200

    improve dc loader

--- library/alnv/CatalogDcAdapter.php
+++ library/alnv/CatalogDcAdapter.php
@@ -13,11 +13,17 @@ class CatalogDcAdapter extends CatalogController {
 
     public function initialize( $strTablename ) {
 
-        if ( in_array( $strTablename, $GLOBALS['TL_CATALOG_MANAGER']['CORE_TABLES'] ) && \Input::get('do') != 'catalog-manager' ) {
+        if ( $this->loadDc( $strTablename ) ) {
 
             $objDcExtractor = new CatalogDcExtractor();
             $objDcExtractor->initialize( $strTablename );
             $GLOBALS['TL_DCA'][ $strTablename ] = $objDcExtractor->convertCatalogToDataContainer();
         }
+    }
+
+
+    protected function loadDc( $strTablename ) {
+
+        return in_array( $strTablename, $GLOBALS['TL_CATALOG_MANAGER']['CORE_TABLES'] ) && ( \Input::get('do') != 'catalog-manager' || \Input::get('table') == 'tl_catalog_fields' );
     }
 }

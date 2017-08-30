3dbe467755aab7f6d688bb8079c066f4b65dd32e
Alexander Naumov <me@alexandernaumov.de>
2017-08-30 09:50:41 +0200

    implement auto completion handler

--- library/alnv/CatalogAutoCompletion.php
+++ library/alnv/CatalogAutoCompletion.php
@@ -2,7 +2,50 @@
 
 namespace CatalogManager;
 
-class CatalogAutoCompletion extends CatalogController {
+class CatalogAutoCompletion extends OptionsGetter {
 
-    //
+
+    public function getOptions() {
+
+        switch ( $this->arrField['autoCompletionType'] ) {
+
+            case 'useDbOptions':
+
+                return $this->getDbOptions();
+
+                break;
+        }
+
+        return [];
+    }
+
+
+    public function getTableEntities() {
+
+        switch ( $this->arrField['autoCompletionType'] ) {
+
+            case 'useDbOptions':
+
+                if ( !$this->arrField['dbTable'] || !$this->arrField['dbTableKey'] ) {
+
+                    return null;
+                }
+
+                if ( !$this->SQLQueryHelper->SQLQueryBuilder->Database->tableExists( $this->arrField['dbTable'] ) ) {
+
+                    return null;
+                }
+
+                if ( !$this->SQLQueryHelper->SQLQueryBuilder->Database->fieldExists( $this->arrField['dbTableKey'], $this->arrField['dbTable'] ) ) {
+
+                    return null;
+                }
+
+                return $this->getResults( true );
+
+                break;
+        }
+
+        return null;
+    }
 }

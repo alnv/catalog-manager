3f4731017191d84a773f0c34ae87464b4bc9f070
Alexander Naumov <me@alexandernaumov.de>
2017-10-13 16:34:14 +0200

    add value setter widget

--- config/config.php
+++ config/config.php
@@ -90,6 +90,7 @@ $GLOBALS['TL_PERMISSIONS'][] = 'filterformp';
 $GLOBALS['BE_FFL']['catalogMessageWidget'] = 'CatalogManager\CatalogMessageWidget';
 $GLOBALS['BE_FFL']['catalogTaxonomyWizard'] = 'CatalogManager\CatalogTaxonomyWizard';
 $GLOBALS['BE_FFL']['catalogTextFieldWidget'] = 'CatalogManager\CatalogTextFieldWidget';
+$GLOBALS['BE_FFL']['catalogValueSetterWizard'] = 'CatalogManager\CatalogValueSetterWizard';
 $GLOBALS['BE_FFL']['catalogDuplexSelectWizard'] = 'CatalogManager\CatalogDuplexSelectWizard';
 $GLOBALS['BE_FFL']['catalogRelationRedirectWizard'] = 'CatalogManager\CatalogRelationRedirectWizard';
 $GLOBALS['BE_FFL']['catalogFilterFieldSelectWizard'] = 'CatalogManager\CatalogFilterFieldSelectWizard';

c0815c527968be29c63b1c25562114a0f574485f
Alexander Naumov <me@alexandernaumov.de>
2017-10-17 12:57:25 +0200

    add social sharing labels

--- languages/en/tl_content.php
+++ languages/en/tl_content.php
@@ -1,3 +1,10 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_content']['social_sharing_legend'] = 'Social-Sharing settings';
+
 $GLOBALS['TL_LANG']['tl_content']['catalogForm'] = [ 'Form', 'Please select a form.' ];
+$GLOBALS['TL_LANG']['tl_content']['catalogSocialSharingTable'] = [ 'Table', 'Please select a table from which the data for the social sharing buttons should be loaded.' ];
+$GLOBALS['TL_LANG']['tl_content']['catalogSocialSharingDescription'] = [ 'Description column', 'Please select a column from which the description should be generated.' ];
+$GLOBALS['TL_LANG']['tl_content']['catalogSocialSharingTitle'] = [ 'Title column', 'Please select a column from which the title should be generated.' ];
+$GLOBALS['TL_LANG']['tl_content']['catalogSocialSharingButtons'] = [ 'Social networks', 'Please select one or more social networks.' ];
+$GLOBALS['TL_LANG']['tl_content']['catalogSocialSharingTemplate'] = [ 'Template', 'Please select a template.' ];

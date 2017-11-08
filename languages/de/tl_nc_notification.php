16f8804cbc1b9635d6e40bb0604bbfb8d35a7825
Alexander Naumov <me@alexandernaumov.de>
2017-11-08 15:42:47 +0100

    add duplicate label

--- languages/de/tl_nc_notification.php
+++ languages/de/tl_nc_notification.php
@@ -4,3 +4,4 @@ $GLOBALS['TL_LANG']['tl_nc_notification']['type']['catalog_manager'] = 'Catalog 
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_delete'] = [ 'Frontend Editing löschen', 'Dieser Benachrichtigungstyp wird nach löschen des Datensatzes versendet.' ];
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_insert'] = [ 'Frontend Editing erstellen', 'Dieser Benachrichtigungstyp wird nach erstellen des Datensatzes versendet.' ];
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_update'] = [ 'Frontend Editing bearbeiten', 'Dieser Benachrichtigungstyp wird nach bearbeiten des Datensatzes versendet.' ];
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_duplicate'] = [ 'Frontend Editing duplizieren', 'Dieser Benachrichtigungstyp wird nach duplizieren des Datensatzes versendet.' ];
--- languages/en/tl_nc_notification.php
+++ languages/en/tl_nc_notification.php
@@ -4,3 +4,4 @@ $GLOBALS['TL_LANG']['tl_nc_notification']['type']['catalog_manager'] = 'Catalog 
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_insert'] = [ 'Frontend Editing: Create', 'This notification type can be sent when the entity is created.' ];
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_update'] = [ 'Frontend Editing: Update', 'This notification type can be sent when the entity is updated.' ];
 $GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_delete'] = [ 'Frontend Editing: Delete', 'This notification type can be sent when the entity is deleted.' ];
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['ctlg_entity_status_duplicate'] = [ 'Frontend Editing: Duplicate', 'This notification type can be sent when the entity is duplicated.' ];

d179e674ca7870045fb187b68f0701b2c1df4e34
Alexander Naumov <me@alexandernaumov.de>
2017-11-23 15:34:07 +0100

    fix problem with multiple values

--- library/alnv/Widgets/CatalogTextFieldWidget.php
+++ library/alnv/Widgets/CatalogTextFieldWidget.php
@@ -105,61 +105,59 @@ class CatalogTextFieldWidget extends \Widget {
             $this->arrOptions = [[]];
         }
 
-        if ( !$this->multiple ) {
+        if ( $this->rgxp == 'url' ) $this->varValue = \Idna::decode( $this->varValue );
+        elseif ( $this->rgxp == 'email' || $this->rgxp == 'friendly' ) $this->varValue = \Idna::decodeEmail( $this->varValue );
 
-            if ( $this->rgxp == 'url' ) $this->varValue = \Idna::decode( $this->varValue );
-            elseif ( $this->rgxp == 'email' || $this->rgxp == 'friendly' ) $this->varValue = \Idna::decodeEmail( $this->varValue );
+        return sprintf( '<input type="%s" name="%s" id="ctrl_%s" class="tl_text%s %s ctlg_awesomplete" value="%s"%s list="ctrl_dl_%s" data-startswith="%s"%s onfocus="Backend.getScrollOffset()">%s%s',
 
-            return sprintf( '<input type="%s" name="%s" id="ctrl_%s" class="tl_text%s ctlg_awesomplete" value="%s"%s list="ctrl_dl_%s" data-startswith="%s"%s onfocus="Backend.getScrollOffset()">%s%s',
-
-                $strType,
-                $this->strName,
-                $this->strId,
-                ( ( $this->strClass != '' ) ? ' ' . $this->strClass : '' ),
-                specialchars( $this->varValue ),
-                $this->getAttributes(),
-                $this->strId,
-                ( $this->startswith ?: '' ),
-                ( $this->multipleValues ? ' data-multiple="1"' : '' ),
-                $this->wizard,
-                $this->generateDataList());
-        }
-
-        if ( !$this->size ) return '';
-        if ( !is_array( $this->varValue ) ) $this->varValue = [ $this->varValue ];
-
-        $arrFields = [];
-
-        for ( $i=0; $i < $this->size; $i++ ) {
-
-            $arrFields[] = sprintf('<input type="%s" name="%s[]" id="ctrl_%s" class="tl_text_%s" value="%s"%s%s onfocus="Backend.getScrollOffset()">',
-
-                $strType,
-                $this->strName,
-                $this->strId.'_'.$i,
-                $this->size,
-                specialchars( @$this->varValue[ $i ] ),
-                $this->getAttributes(),
-                ( $this->multipleValues ? ' data-multiple="1"' : '' )
-            );
-        }
-
-        return sprintf('<div id="ctrl_%s"%s>%s</div>%s',
-
+            $strType,
+            $this->strName,
             $this->strId,
-            ( ($this->strClass != '' ) ? ' class="' . $this->strClass . '"' : ''),
-            implode( ' ', $arrFields ),
-            $this->wizard);
+            ( ( $this->strClass != '' ) ? ' ' . $this->strClass : '' ),
+            ( version_compare(VERSION, '4.0', '>=') ? '_contao4' : '_contao3' ),
+            specialchars( $this->varValue ),
+            $this->getAttributes(),
+            $this->strId,
+            ( $this->startswith ?: '' ),
+            ( $this->multiple ? ' data-multiple="1"' : '' ),
+            $this->wizard,
+            $this->generateDataList());
+    }
+
+
+    protected function flatOptions() {
+
+        $arrOptions = [];
+
+        foreach ( $this->arrOptions as $arrOption ) {
+
+            if ( !is_array( $arrOption ) ) continue;
+
+            if ( Toolkit::isEmpty( $arrOption['value'] ) || !is_string( $arrOption['value'] ) ) continue;
+
+            $arrValues = explode( ',', $arrOption['value'] );
+
+            if ( is_array( $arrValues ) ) {
+
+                foreach ( $arrValues as $strValue ) {
+
+                    if ( !in_array( $strValue, $arrOptions ) ) $arrOptions[] = $strValue;
+                }
+            }
+        }
+
+        return $arrOptions;
     }
 
 
     protected function generateDataList() {
 
         $strOptions = '';
+        $arrOptions = $this->flatOptions();
 
-        foreach ( $this->arrOptions as $arrOption ) {
+        foreach ( $arrOptions as $strOption ) {
 
-            $strOptions .= sprintf( '<option value="%s">%s</option>', $arrOption['value'], $arrOption['label'] );
+            $strOptions .= sprintf( '<option value="%s">%s</option>', $strOption, $strOption );
         }
 
         return sprintf( '<datalist id="ctrl_dl_%s">%s</datalist>',

25aa3b8c8b50e4709f87e3708f992fd194771396
Alexander Naumov <me@alexandernaumov.de>
2017-12-08 16:46:32 +0100

    implement array handler

--- library/alnv/Fields/Text.php
+++ library/alnv/Fields/Text.php
@@ -48,10 +48,13 @@ class Text {
 
 
     public static function parseValue( $varValue, $arrField, $arrCatalog ) {
-        
-        if ( Toolkit::isEmpty( $varValue ) ) return $arrField['multiple'] ? [] : '';
 
-        if ( $arrField['multiple'] ) {
+        $varValue = deserialize( $varValue );
+        
+        if ( Toolkit::isEmpty( $varValue ) && is_string( $varValue ) ) return '';
+        if ( is_array( $varValue ) && empty( $varValue ) ) return [];
+
+        if ( is_array( $varValue ) || $arrField['multiple'] ) {
 
             $arrReturn = [];
             $varValue = Toolkit::parseMultipleOptions( $varValue );

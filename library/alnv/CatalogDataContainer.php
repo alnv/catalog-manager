daadb29e8cc2b45d22f0ea1418fe829beb2db21c
Alexander Naumov <me@alexandernaumov.de>
2017-12-21 12:40:03 +0100

    implement catalog data container

---
+++ library/alnv/CatalogDataContainer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CatalogManager;
+
+class CatalogDataContainer extends \DataContainer {
+
+
+    public function __construct( $strTable ) {
+
+        parent::__construct();
+
+        $this->table = $strTable;
+    }
+
+
+    public function __set( $strKey, $varValue ) {
+
+        switch ( $strKey ) {
+
+            case 'activeRecord':
+
+                $objEntity = null;
+
+                if ( isset( $varValue['id'] ) && $varValue['id'] ) {
+
+                    $objEntity = $this->Database->prepare( sprintf( 'SELECT * FROM %s WHERE id = ?', $this->table ) )->limit(1)->execute( $varValue['id'] );
+                }
+
+                $this->objActiveRecord = $objEntity;
+
+                break;
+
+            default;
+
+                parent::__set( $strKey, $varValue );
+
+                break;
+        }
+    }
+
+
+    public function getPalette(){}
+    public function save( $varValue ){}
+}

3dcc1340531577e1257cbfa16d6a04f969c9bdff
Alexander Naumov <me@alexandernaumov.de>
2018-05-10 18:48:51 +0200

    add missing labels

--- languages/de/tl_page.php
+++ languages/de/tl_page.php
@@ -9,5 +9,6 @@ $GLOBALS['TL_LANG']['tl_page']['catalogUseRouting'] = [ 'Routing verwenden', 'Hi
 $GLOBALS['TL_LANG']['tl_page']['catalogRoutingParameter'] = [ 'Parameter auswählen', 'Bitte wählen Sie Ihre Routing Parameter aus.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogSetAutoItem'] = [ 'Auto Item verwenden', 'Hier können Sie einen auto_item Parameter anhängen.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogUseMaster'] = [ 'Master verwenden', 'Hier können Sie einen Master-Datensatz für Ihre Taxonomy-Einstellungen auswählen.' ];
+$GLOBALS['TL_LANG']['tl_page']['catalogShowInBreadcrumb'] = [ 'Seite im Breadcrumb anzeigen', 'Hier können Sie die Seite im Breadcrumb zusätzlich zu der Detailseite anzeigen.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogUseChangeLanguage'] = [ 'Einträge übersetzen', 'Diese Einstellung erlaubt es Ihnen die Einträge auf einer Detailseite via Sprachwechsler Modul zu wechseln.' ];
 $GLOBALS['TL_LANG']['tl_page']['catalogRouting'] = [ 'Routing Parameter', 'Hier können Sie die ausgewählten Routing Parameter überprüfen. Diese Routing Parameter werden bei der Alias Generierung berücksichtigt.' ];

6444870c5f852fa80f3c0246a0d5031899b1b71d
Alexander Naumov <me@alexandernaumov.de>
2018-07-10 13:49:07 +0200

    impl visibility content elements

---
+++ library/alnv/Elements/ContentVisibilityPanelStart.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CatalogManager;
+
+class ContentVisibilityPanelStart extends \ContentElement {
+
+
+    public function generate() {
+
+        return parent::generate();
+    }
+
+
+    protected function compile() {}
+}
---
+++ library/alnv/Elements/ContentVisibilityPanelStop.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CatalogManager;
+
+class ContentVisibilityPanelStop extends \ContentElement {
+
+
+    public function generate() {
+
+        return parent::generate();
+    }
+
+
+    protected function compile() {}
+}

e15ad7133b763a557104c296943a25b25a6355ed
Alexander Naumov <me@alexandernaumov.de>
2019-03-20 12:55:36 +0100

    generate be tpl

--- library/alnv/Elements/ContentVisibilityPanelStart.php
+++ library/alnv/Elements/ContentVisibilityPanelStart.php
@@ -7,6 +7,14 @@ class ContentVisibilityPanelStart extends \ContentElement {
 
     public function generate() {
 
+        if ( TL_MODE == 'BE' ) {
+
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '### ' . strtoupper( $GLOBALS['TL_LANG']['CTE']['catalogVisibilityPanelStart'][0] ) . ' ###';
+
+            return $objTemplate->parse();
+        }
+
         return parent::generate();
     }
 
--- library/alnv/Elements/ContentVisibilityPanelStop.php
+++ library/alnv/Elements/ContentVisibilityPanelStop.php
@@ -7,6 +7,14 @@ class ContentVisibilityPanelStop extends \ContentElement {
 
     public function generate() {
 
+        if ( TL_MODE == 'BE' ) {
+
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '### ' . strtoupper( $GLOBALS['TL_LANG']['CTE']['catalogVisibilityPanelStop'][0] ) . ' ###';
+
+            return $objTemplate->parse();
+        }
+
         return parent::generate();
     }
 

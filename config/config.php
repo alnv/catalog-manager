cc4915f9bf5c2e51bbfe4db5d4bf889ce0ced494
Alexander Naumov <me@alexandernaumov.de>
2019-10-23 19:42:47 +0200

    change version

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define( "CATALOG_MANAGER_VERSION", "1.31.16" );
+define( "CATALOG_MANAGER_VERSION", "1.31.17" );
 
 if ( !\Config::get('hideCatalogManager') ) {
     array_insert( $GLOBALS['BE_MOD'], 3, [

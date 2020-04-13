81b89b72d7d0677464db92892ce6ee13c149612f
Alexander Naumov <mail@alexandernaumov.de>
2020-04-13 15:51:10 +0200

    fix #241

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define( "CATALOG_MANAGER_VERSION", "1.31.25" );
+define( "CATALOG_MANAGER_VERSION", "1.31.26" );
 
 if ( !\Config::get('hideCatalogManager') ) {
     array_insert( $GLOBALS['BE_MOD'], 3, [

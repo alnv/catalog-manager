5a02ff65f3c1613a902bceee286edad57cae9a1d
Alexander Naumov <alexandernaumov@Alexander-Naumov-Geschaft.local>
2022-09-16 17:56:06 +0200

    add contao-community-alliance/composer-plugin

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("CATALOG_MANAGER_VERSION", "1.32.15");
+define("CATALOG_MANAGER_VERSION", "1.32.16");
 
 if (!\Config::get('hideCatalogManager')) {
     array_insert( $GLOBALS['BE_MOD'], 3, [

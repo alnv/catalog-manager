53462423a6ea4c81e39ab1c8c452b742e669b698
Benedict Massolle <bm@presentprogressive.de>
2023-09-06 09:00:16 +0200

    Fix undefined array indizes in catalog dcas

--- dca/tl_catalog.php
+++ dca/tl_catalog.php
@@ -54,6 +54,7 @@ $GLOBALS['TL_DCA']['tl_catalog'] = [
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
                 'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\'))return false;Backend.getScrollOffset()"'
+
             ],
             'show' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_catalog']['show'],

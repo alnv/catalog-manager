832891a4155fa71e63c0ad1502a81e73eb083091
Alexander Naumov <alexandernaumov@MacBookPro.fritz.box>
2025-05-23 14:04:46 +0200

    fx

--- contao/config/config.php
+++ contao/config/config.php
@@ -38,7 +38,7 @@ use Contao\ArrayUtil;
 use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 
-const CATALOG_MANAGER_VERSION = "2.0.14-legacy";
+const CATALOG_MANAGER_VERSION = "2.0.15-legacy";
 
 ArrayUtil::arrayInsert($GLOBALS['BE_MOD'], 3, [
     'catalog-manager-extensions' => [
--- src/SearchIndexBuilder.php
+++ src/SearchIndexBuilder.php
@@ -12,16 +12,15 @@ use Contao\System;
 class SearchIndexBuilder extends Frontend
 {
 
-    protected array $arrRoutings = [];
-
     public function initialize($arrPages, $intRoot = 0, $blnIsSitemap = false)
     {
 
         $arrRoot = [];
-        $this->arrRoutings = [];
         $this->import(SQLQueryBuilder::class, 'SQLQueryBuilder');
 
-        if ($intRoot > 0) $arrRoot = $this->Database->getChildRecords($intRoot, 'tl_page');
+        if ($intRoot > 0) {
+            $arrRoot = $this->Database->getChildRecords($intRoot, 'tl_page');
+        }
 
         $arrProcessed = [];
         $objModules = $this->Database->prepare('SELECT * FROM tl_module WHERE type=?')->execute('catalogUniversalView');
@@ -32,20 +31,22 @@ class SearchIndexBuilder extends Frontend
             if (!$objModules->catalogMasterPage) continue;
             if (!$objModules->catalogTablename) continue;
 
-            if (!empty($arrRoot) && !in_array($objModules->catalogMasterPage, $arrRoot)) continue;
+            if (!empty($arrRoot) && !\in_array($objModules->catalogMasterPage, $arrRoot)) continue;
 
             if (!isset($arrProcessed[$objModules->catalogMasterPage])) {
 
                 $objParent = $this->getPageModelWithDetailsByID($objModules->catalogMasterPage, $objModules, $blnIsSitemap);
-
-                if ($objParent === null) continue;
+                if (!$objParent) {
+                    continue;
+                }
 
                 $arrProcessed[$objModules->catalogMasterPage] = $this->setProcessedDomain($objParent, $objModules->catalogTablename);
             }
 
             $objCatalog = $this->Database->prepare('SELECT * FROM tl_catalog WHERE tablename=?')->limit(1)->execute($objModules->catalogTablename);
-
-            if (!$objCatalog->numRows) continue;
+            if (!$objCatalog->numRows) {
+                continue;
+            }
 
             $arrCatalog = Toolkit::parseCatalog($objCatalog->row());
 
@@ -54,18 +55,17 @@ class SearchIndexBuilder extends Frontend
                 'table' => $objModules->catalogTablename
             ];
 
-            $strUrl = $arrProcessed[$objModules->catalogMasterPage];
-            $strQuery = sprintf('SELECT * FROM %s', $objModules->catalogTablename);
+            $strUrl = $arrProcessed[$objModules->catalogMasterPage] ?? '';
+            $strQuery = \sprintf('SELECT * FROM %s', $objModules->catalogTablename);
 
             if ($objModules->type == 'catalogUniversalView' && $objModules->catalogTaxonomies) {
                 $arrTaxonomies = Toolkit::parseStringToArray($objModules->catalogTaxonomies);
-
-                if (is_array($arrTaxonomies) && isset($arrTaxonomies['query'])) {
+                if (\is_array($arrTaxonomies) && isset($arrTaxonomies['query'])) {
                     $arrQuery['where'] = Toolkit::parseQueries($arrTaxonomies['query']);
                 }
             }
 
-            if (is_array($arrCatalog['operations']) && in_array('invisible', $arrCatalog['operations'])) {
+            if (\is_array($arrCatalog['operations']) && \in_array('invisible', $arrCatalog['operations'])) {
                 $dteTime = Date::floorToMinute();
                 $arrQuery['where'][] = [
                     [
@@ -102,9 +102,9 @@ class SearchIndexBuilder extends Frontend
             $strQuery = $strQuery . $this->SQLQueryBuilder->getWhereQuery($arrQuery);
             $arrValues = $this->SQLQueryBuilder->getValues();
 
-            if (isset($GLOBALS['TL_HOOKS']['catalogManagerGetSearchablePagesQuery']) && is_array($GLOBALS['TL_HOOKS']['catalogManagerGetSearchablePagesQuery'])) {
+            if (isset($GLOBALS['TL_HOOKS']['catalogManagerGetSearchablePagesQuery']) && \is_array($GLOBALS['TL_HOOKS']['catalogManagerGetSearchablePagesQuery'])) {
                 foreach ($GLOBALS['TL_HOOKS']['catalogManagerGetSearchablePagesQuery'] as $arrCallback) {
-                    if (is_array($arrCallback)) {
+                    if (\is_array($arrCallback)) {
                         $this->import($arrCallback[0]);
                         $strQuery = $this->{$arrCallback[0]}->{$arrCallback[1]}($strQuery, $objModules->catalogTablename, $arrQuery);
                     }
@@ -112,41 +112,43 @@ class SearchIndexBuilder extends Frontend
             }
 
             $objEntities = $this->Database->prepare($strQuery)->execute(...$arrValues);
-
-            if (!$objEntities->numRows) continue;
+            if (!$objEntities->numRows) {
+                continue;
+            }
 
             while ($objEntities->next()) {
-                $strSiteMapUrl = $this->createMasterUrl($arrCatalog, $objEntities, $strUrl, $objModules->catalogTablename);
-                if (isset($GLOBALS['TL_HOOKS']['catalogManagerAlterSitemapUrl']) && is_array($GLOBALS['TL_HOOKS']['catalogManagerAlterSitemapUrl'])) {
+
+                $strSiteMapUrl = $this->createMasterUrl($arrCatalog, $objEntities, $strUrl);
+                if (isset($GLOBALS['TL_HOOKS']['catalogManagerAlterSitemapUrl']) && \is_array($GLOBALS['TL_HOOKS']['catalogManagerAlterSitemapUrl'])) {
                     foreach ($GLOBALS['TL_HOOKS']['catalogManagerAlterSitemapUrl'] as $arrCallback) {
-                        if (is_array($arrCallback)) {
+                        if (\is_array($arrCallback)) {
                             $this->import($arrCallback[0]);
                             $strSiteMapUrl = $this->{$arrCallback[0]}->{$arrCallback[1]}($strSiteMapUrl, $objModules->catalogTablename, $strUrl);
                         }
                     }
                 }
 
-                if ($strSiteMapUrl && !in_array($strSiteMapUrl, $arrPages)) $arrPages[] = $strSiteMapUrl;
+                if ($strSiteMapUrl && !\in_array($strSiteMapUrl, $arrPages)) {
+                    $arrPages[] = $strSiteMapUrl;
+                }
             }
         }
 
         return $arrPages;
     }
 
-    protected function createMasterUrl($arrCatalog, $objEntities, $strUrl, $strTablename): string
+    protected function createMasterUrl($arrCatalog, $objEntities, $strUrl): string
     {
 
-        $strBase = '';
-        $strUrl = rawurldecode($strUrl);
-        $strRootDir = System::getContainer()->getParameter('kernel.project_dir');
+        $strUrl = \rawurldecode($strUrl);
 
         if ($arrCatalog['useRedirect'] && $arrCatalog['internalUrlColumn']) {
             if ($objEntities->{$arrCatalog['internalUrlColumn']}) {
+
                 $intPageID = intval(preg_replace('/[^0-9]+/', '', $objEntities->{$arrCatalog['internalUrlColumn']}));
                 $objParent = $this->getPageModelWithDetailsByID($intPageID);
-                $strDomain = ($objParent->rootUseSSL ? 'https://' : 'http://') . ($objParent->domain ?: Environment::get('host')) . $strRootDir . '/';
 
-                return $strDomain . $objParent->getFrontendUrl();
+                return $objParent->getAbsoluteUrl();
             }
         }
 
@@ -156,17 +158,9 @@ class SearchIndexBuilder extends Frontend
             }
         }
 
-        $arrParameters = [];
+        $arrParameters = [$objEntities->alias];
 
-        if (isset($this->arrRoutings[$strTablename]) && is_array($this->arrRoutings[$strTablename])) {
-            foreach ($this->arrRoutings[$strTablename] as $strParameter) {
-                $arrParameters[] = $objEntities->{$strParameter} ?: ' ';
-            }
-        }
-
-        $arrParameters[] = ($objEntities->alias != '' && !Config::get('disableAlias')) ? $objEntities->alias : $objEntities->id;
-
-        return $strBase . vsprintf($strUrl, $arrParameters);
+        return \vsprintf($strUrl, $arrParameters);
     }
 
     protected function getPageModelWithDetailsByID($intPageID, $objCatalog = null, $blnIsSitemap = false)
@@ -187,21 +181,6 @@ class SearchIndexBuilder extends Frontend
 
     protected function setProcessedDomain($objPage, $strTablename): string
     {
-
-        $strRoutings = '';
-        $strRootDir = System::getContainer()->getParameter('kernel.project_dir');
-        $strDomain = ($objPage->rootUseSSL ? 'https://' : 'http://') . ($objPage->domain ?: Environment::get('host')) . $strRootDir . '/';
-
-        if ($objPage->catalogUseRouting) {
-            $this->arrRoutings[$strTablename] = Toolkit::getRoutingParameter($objPage->catalogRouting);
-            if (in_array('auto_item', $this->arrRoutings[$strTablename])) unset($this->arrRoutings[$strTablename]['auto_item']);
-        }
-
-        if (isset($this->arrRoutings[$strTablename]) && is_array($this->arrRoutings[$strTablename])) {
-            $arrRoutings = array_keys($this->arrRoutings[$strTablename]);
-            $strRoutings = implode('', array_fill(0, count($arrRoutings), '/%s'));
-        }
-
-        return $strDomain . $objPage->getFrontendUrl(((Config::get('useAutoItem') && !Config::get('disableAlias')) ? $strRoutings . '/%s' : $strRoutings . '/items/%s'));
+        return $objPage->getAbsoluteUrl('/%s');
     }
 }
